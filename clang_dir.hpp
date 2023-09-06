c8c64d624cf2a2c8c42e164cd8dffdc9ee5e15e0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-06 11:00:02 +0100

    clang_dir: avoiding strdup
    uses a static char buffer to avoid the requirement for strdup. In build,
    this would be fine, but it isn't in leco since it might call it multiple
    times.

--- clang_dir.hpp
+++ clang_dir.hpp
@@ -5,7 +5,7 @@
 
 #if _WIN32
 const char *clang_dir() {
-  char buf[1024];
+  static char buf[1024];
   auto f = _popen("where clang++", "r");
   if (fgets(buf, 1024, f) == nullptr)
     throw 0;
@@ -13,16 +13,16 @@ const char *clang_dir() {
   buf[strlen(buf)] = 0;
   *strrchr(buf, '\\') = 0;
   *strrchr(buf, '\\') = 0;
-  return _strdup(buf);
+  return buf;
 }
 #else
 const char *clang_dir() {
-  char buf[1024];
+  static char buf[1024];
   auto f = popen("brew --prefix llvm@16", "r");
   if (fgets(buf, 1024, f) == nullptr)
     throw 0;
 
   buf[strlen(buf)] = 0;
-  return _strdup(buf);
+  return buf;
 }
 #endif
