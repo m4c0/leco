b5b172fff3785b08f17e96ad0017a41faa0ba751
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-06 09:51:25 +0100

    using driver since cc1 directly is not a good idea

--- compile.cpp
+++ compile.cpp
@@ -1,10 +1,19 @@
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "clang_dir.hpp"
 #include "clang/CodeGen/CodeGenAction.h"
+#include "clang/Driver/Compilation.h"
+#include "clang/Driver/Driver.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/TargetParser/Host.h"
 
 using namespace clang;
+using namespace clang::driver;
 using namespace llvm;
 
 DiagnosticsEngine &diag_engine() {
@@ -26,16 +35,47 @@ const char *clang_exe() {
   return exe.data();
 }
 
-bool compile(StringRef path) {
-  errs() << "compiling " << path << "\n";
+Driver &dvr() {
+  static auto drv = [] {
+    auto def_triple = sys::getDefaultTargetTriple();
+    return Driver{clang_exe(), def_triple, diag_engine()};
+  }();
+  return drv;
+}
 
-  SmallVector<const char *> args{"-v"};
-
+auto cc1(SmallVectorImpl<const char *> args) {
   auto cinst = std::make_unique<CompilerInstance>();
   CompilerInvocation::CreateFromArgs(cinst->getInvocation(), args,
                                      diag_engine(), clang_exe());
   cinst->createDiagnostics();
 
   EmitObjAction a{};
-  return cinst->ExecuteAction(a);
+  return !cinst->ExecuteAction(a);
+}
+
+bool compile(StringRef file) {
+  errs() << "compiling " << file << "\n";
+
+  auto path = sys::path::parent_path(file);
+  auto name = sys::path::stem(file);
+  auto ext = sys::path::extension(file);
+  SmallString<128> obj{};
+  sys::path::append(obj, path, "out", name);
+  sys::path::replace_extension(obj, ext + ".o");
+
+  SmallVector<const char *> args{
+      {"-std=c++20", "-c", file.data(), "-o", obj.data()}};
+  std::unique_ptr<Compilation> c{dvr().BuildCompilation(args)};
+  if (!c || c->containsError())
+    // We did a mistake in clang args. Bail out and let the diagnostics
+    // client do its job informing the user
+    return false;
+
+  SmallVector<std::pair<int, const ::clang::driver::Command *>, 4> failures;
+  int res = dvr().ExecuteCompilation(*c, failures);
+  for (const auto &p : failures) {
+    if (p.first)
+      return false;
+  }
+  return res == 0;
 }
