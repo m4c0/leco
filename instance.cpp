c208fabcd979f0ef630b072d602e1850861375e7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-07 13:13:01 +0100

    instance: handling prebuilt-module-path for every compilation

--- instance.cpp
+++ instance.cpp
@@ -4,17 +4,28 @@
 using namespace clang;
 using namespace llvm;
 
+StringSet<> &module_paths() {
+  static StringSet<> i{};
+  return i;
+}
+
 instance::instance(std::shared_ptr<CompilerInstance> ci, StringRef out)
     : m_ci{std::move(ci)}, m_output{out.str()} {}
 instance::~instance() = default;
 
 instance &instance::add_module_path(StringRef path) {
-  m_ci->getHeaderSearchOpts().AddPrebuiltModulePath(path);
+  module_paths().insert(path);
   return *this;
 }
 
 bool instance::run(FrontendAction *a) {
-  return m_ci && m_ci->ExecuteAction(*a);
+  if (!m_ci)
+    return false;
+
+  for (auto &p : module_paths())
+    m_ci->getHeaderSearchOpts().AddPrebuiltModulePath(p.first());
+
+  return m_ci->ExecuteAction(*a);
 }
 
 StringRef instance::output() { return m_output; }
