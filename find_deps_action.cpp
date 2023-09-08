8edbcb559097ce2d5b8c192d81047e338a2eaad6
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-08 18:28:58 +0100

    find_deps: expose pp_callback

--- find_deps_action.cpp
+++ find_deps_action.cpp
@@ -6,61 +6,52 @@
 using namespace clang;
 using namespace llvm;
 
-class find_deps_pp_callbacks : public PPCallbacks {
-  DiagnosticsEngine *m_diags;
-  StringRef m_cur_file;
+void find_deps_pp_callbacks::report_compilation_error(SourceLocation loc) {
+  auto lvl = DiagnosticsEngine::Error;
+  auto did = m_diags->getCustomDiagID(lvl, "failed to compile dependency");
+  m_diags->Report(loc, did);
+}
+void find_deps_pp_callbacks::report_missing_module(SourceLocation loc) {
+  auto lvl = DiagnosticsEngine::Error;
+  auto did = m_diags->getCustomDiagID(lvl, "module not found");
+  m_diags->Report(loc, did);
+}
 
-  void report_compilation_error(SourceLocation loc) {
-    auto lvl = DiagnosticsEngine::Error;
-    auto did = m_diags->getCustomDiagID(lvl, "failed to compile dependency");
-    m_diags->Report(loc, did);
-  }
-  void report_missing_module(SourceLocation loc) {
-    auto lvl = DiagnosticsEngine::Error;
-    auto did = m_diags->getCustomDiagID(lvl, "module not found");
-    m_diags->Report(loc, did);
-  }
+void find_deps_pp_callbacks::moduleImport(SourceLocation loc, ModuleIdPath path,
+                                          const Module *imported) {
+  assert(path.size() == 1 && "path isn't atomic");
 
-public:
-  find_deps_pp_callbacks(DiagnosticsEngine *diags, StringRef file)
-      : m_diags{diags}, m_cur_file{file} {}
+  auto dir = sys::path::parent_path(m_cur_file);
 
-  void moduleImport(SourceLocation loc, ModuleIdPath path,
-                    const Module *imported) override {
-    assert(path.size() == 1 && "path isn't atomic");
+  auto &[id, _] = path[0];
+  auto mod_name = id->getName();
+  auto p = mod_name.find(":");
+  SmallString<128> dep{};
+  if (p != StringRef::npos) {
+    auto me = mod_name.substr(0, p);
+    auto part = mod_name.substr(p + 1);
 
-    auto dir = sys::path::parent_path(m_cur_file);
+    sys::path::append(dep, dir, me + "-" + part + ".cppm");
+    if (compile(dep.c_str()))
+      return;
 
-    auto &[id, _] = path[0];
-    auto mod_name = id->getName();
-    auto p = mod_name.find(":");
-    SmallString<128> dep{};
-    if (p != StringRef::npos) {
-      auto me = mod_name.substr(0, p);
-      auto part = mod_name.substr(p + 1);
-
-      sys::path::append(dep, dir, me + "-" + part + ".cppm");
-      if (compile(dep.c_str()))
-        return;
-
-    } else {
-      auto t = mod_name + ".cppm";
-      sys::path::append(dep, dir, t);
-      if (!sys::fs::is_regular_file(dep)) {
-        dep.clear();
-        sys::path::append(dep, "..", mod_name, t);
-      }
-      if (!sys::fs::is_regular_file(dep.c_str()))
-        return report_missing_module(loc);
-
-      if (compile(dep.c_str()))
-        return;
+  } else {
+    auto t = mod_name + ".cppm";
+    sys::path::append(dep, dir, t);
+    if (!sys::fs::is_regular_file(dep)) {
+      dep.clear();
+      sys::path::append(dep, "..", mod_name, t);
     }
+    if (!sys::fs::is_regular_file(dep.c_str()))
+      return report_missing_module(loc);
 
-    report_missing_module(loc);
-    return;
+    if (compile(dep.c_str()))
+      return;
   }
-};
+
+  report_missing_module(loc);
+  return;
+}
 
 void find_deps_action::ExecuteAction() {
   auto *diags = &getCompilerInstance().getDiagnostics();
--- find_deps_action.hpp
+++ find_deps_action.hpp
@@ -2,6 +2,21 @@
 
 #include "clang/Frontend/FrontendActions.h"
 
+class find_deps_pp_callbacks : public clang::PPCallbacks {
+  clang::DiagnosticsEngine *m_diags;
+  llvm::StringRef m_cur_file;
+
+  void report_compilation_error(clang::SourceLocation loc);
+  void report_missing_module(clang::SourceLocation loc);
+
+public:
+  find_deps_pp_callbacks(clang::DiagnosticsEngine *diags, llvm::StringRef file)
+      : m_diags{diags}, m_cur_file{file} {}
+
+  void moduleImport(clang::SourceLocation loc, clang::ModuleIdPath path,
+                    const clang::Module *imported) override;
+};
+
 class find_deps_action : public clang::PreprocessorFrontendAction {
 public:
   void ExecuteAction() override;
