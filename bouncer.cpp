27d6a21ec7006322f54f1e7c839087c73db5510e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-08 19:57:48 +0100

    bouncer: remove preprocessing step
    this should be moved inside a single CI, eventually

--- bouncer.cpp
+++ bouncer.cpp
@@ -1,33 +1,8 @@
 #include "bouncer.hpp"
 #include "compile.hpp"
-#include "evoker.hpp"
-#include "instance.hpp"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/FrontendActions.h"
-#include "clang/Lex/Preprocessor.h"
+#include "llvm/Support/Path.h"
 
-using namespace clang;
 using namespace llvm;
-
-class bouncer : public PreprocessorFrontendAction {
-  bool m_valid{};
-
-public:
-  void ExecuteAction() override {
-    auto *diags = &getCompilerInstance().getDiagnostics();
-    auto &pp = getCompilerInstance().getPreprocessor();
-    pp.EnterMainSourceFile();
-
-    Token Tok;
-    do {
-      pp.Lex(Tok);
-    } while (!Tok.is(tok::eof));
-
-    m_valid = !pp.isInNamedModule();
-  }
-
-  [[nodiscard]] bool is_valid() { return m_valid; }
-};
 
 bool bounce(StringRef path) {
   auto stem = sys::path::stem(path);
@@ -40,9 +15,5 @@ bool bounce(StringRef path) {
   if (ext != ".cpp")
     return true;
 
-  auto b = evoker{}.push_arg("-c").set_inout(path, ".o").build().run<bouncer>();
-  if (b && b->is_valid())
-    return compile(path); // TODO: link as well
-
-  return true;
+  return compile(path); // TODO: link as well
 }
