fa47c1f1705a6a98a37104078984eac7595b5570
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-09 12:54:57 +0100

    cl: option for cleanup

--- cl.cpp
+++ cl.cpp
@@ -3,7 +3,15 @@
 
 using namespace llvm;
 
-cl::OptionCategory leco_cat("Leco Options", "Options exclusive to LECO");
+cl::OptionCategory leco_cat("Leco Options");
+
+enum clean_levels { cup_none, cup_cur, cup_all };
+cl::opt<clean_levels> clean_level(
+    "clean", cl::desc("Cleaning level before compilation:"),
+    cl::values(clEnumValN(cup_none, "none", "No cleanup, do incremental build"),
+               clEnumValN(cup_cur, "cur", "Cleanup current directory"),
+               clEnumValN(cup_all, "all", "Cleanup any traversed directory")),
+    cl::cat(leco_cat));
 
 void parse_args(int argc, char **argv) {
   for (auto &[k, v] : cl::getRegisteredOptions()) {
