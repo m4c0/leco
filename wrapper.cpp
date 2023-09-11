dcfaddfad109481d1fa54393aecfc3a2a4ab50b3
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-11 13:11:15 +0100

    wrapper: cleanup

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,5 +1,3 @@
-#include "compile.hpp"
-#include "context.hpp"
 #include "find_deps_action.hpp"
 #include "p_add_impl.hpp"
 #include "wrapper.hpp"
@@ -8,16 +6,6 @@
 using namespace clang;
 using namespace llvm;
 
-class tool_pragma_handler : public PragmaHandler {
-public:
-  tool_pragma_handler() : PragmaHandler{"tool"} {}
-
-  void HandlePragma(Preprocessor &PP, PragmaIntroducer Introducer,
-                    Token &PragmaTok) {
-    cur_ctx().tool = true;
-  }
-};
-
 bool wrapper_action::BeginSourceFileAction(CompilerInstance &ci) {
   auto *diags = &ci.getDiagnostics();
   auto &pp = ci.getPreprocessor();
@@ -25,7 +13,6 @@ bool wrapper_action::BeginSourceFileAction(CompilerInstance &ci) {
   pp.addPPCallbacks(
       std::make_unique<find_deps_pp_callbacks>(diags, getCurrentFile()));
 
-  pp.AddPragmaHandler("leco", new tool_pragma_handler());
   pp.AddPragmaHandler("leco", new add_impl_pragma_handler(getCurrentFile()));
 
   return WrapperFrontendAction::BeginSourceFileAction(ci);
