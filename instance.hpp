13a1cf05a249d70ca0a1176f8d8ad68d06766cba
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-15 11:45:23 +0100

    instance: merging unit into header

--- instance.hpp
+++ instance.hpp
@@ -1,8 +1,8 @@
 #pragma once
+#include "clang/Frontend/CompilerInstance.h"
 #include "llvm/ADT/SmallString.h"
 
 namespace clang {
-class CompilerInstance;
 class FrontendAction;
 } // namespace clang
 
@@ -10,13 +10,16 @@ struct context;
 class instance {
   std::shared_ptr<clang::CompilerInstance> m_ci;
   std::string m_output;
+  // m_ci->getHeaderSearchOpts().AddPrebuiltModulePath(p);
 
 public:
   explicit instance(std::shared_ptr<clang::CompilerInstance> ci,
-                    llvm::StringRef out);
-  ~instance();
+                    llvm::StringRef out)
+      : m_ci{std::move(ci)}, m_output{out.str()} {}
 
-  [[nodiscard]] bool run(std::unique_ptr<clang::FrontendAction> a);
+  [[nodiscard]] bool run(std::unique_ptr<clang::FrontendAction> a) {
+    return m_ci ? m_ci->ExecuteAction(*a) : false;
+  }
 
   template <typename Tp> bool run() { return run(std::make_unique<Tp>()); }
 };
