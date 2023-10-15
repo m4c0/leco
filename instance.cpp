bdeafba128d054c6361ac81f28af31b6d193ee2b
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-15 10:59:56 +0100

    🐞 instance: cleanup

--- instance.cpp
+++ instance.cpp
@@ -5,62 +5,21 @@
 using namespace clang;
 using namespace llvm;
 
-auto &cleaned_up_paths() {
-  static std::set<std::string> i{};
-  return i;
-}
-auto &current_paths() {
-  static std::set<std::string> i{};
-  return i;
-}
-auto &in_flights() {
-  static std::vector<std::shared_ptr<CompilerInstance>> i{};
-  return i;
-}
-
-void clear_module_path_cache() { current_paths().clear(); }
-
 instance::instance(std::shared_ptr<CompilerInstance> ci, StringRef out)
     : m_ci{std::move(ci)}, m_output{out.str()} {
 
-  auto &inf = in_flights();
-
   if (m_ci) {
-    for (auto &p : current_paths()) {
-      m_ci->getHeaderSearchOpts().AddPrebuiltModulePath(p);
-    }
+    // m_ci->getHeaderSearchOpts().AddPrebuiltModulePath(p);
 
     if (out != "" && is_verbose()) {
       errs() << "compiling " << out << "\n";
     }
-    inf.push_back(m_ci);
   }
 
   auto path = sys::path::parent_path(out);
-  auto [_, inserted] = current_paths().insert(path.str());
-
-  if (!inserted)
-    return;
-
-  auto [__, not_cleaned] = cleaned_up_paths().insert(path.str());
-  if (not_cleaned) {
-    if ((should_clean_current() && cleaned_up_paths().size() == 1) ||
-        should_clean_all()) {
-      if (is_verbose())
-        errs() << "cleaning up " << path << "\n";
-      sys::fs::remove_directories(path);
-    }
-  }
   sys::fs::create_directories(path);
-
-  for (auto &ci : inf) {
-    ci->getHeaderSearchOpts().AddPrebuiltModulePath(path);
-  }
 }
-instance::~instance() {
-  if (m_ci)
-    std::erase(in_flights(), m_ci);
-}
+instance::~instance() {}
 
 bool instance::run(std::unique_ptr<FrontendAction> a) {
   return m_ci ? m_ci->ExecuteAction(*a) : false;
