d45ab05dfe9ee3fc15d0857ebded19a27245fb7d
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-26 18:30:34 +0100

    actool: generate xcasset's jsons

--- actool.cpp
+++ actool.cpp
@@ -1,10 +1,70 @@
 #include "actool.hpp"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/Twine.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
+
+namespace json {
+class dict {
+  raw_ostream &o;
+
+  void array_elements() {}
+  void array_elements(auto &&v) {
+    o << "{";
+    v(dict{o});
+    o << "}";
+  }
+  void array_elements(auto &&v1, auto &&v2, auto &&...vs) {
+    array_elements(v1);
+    o << ",";
+    array_elements(v2, vs...);
+  }
+
+public:
+  explicit constexpr dict(raw_ostream &o) : o{o} {}
+
+  void array(StringRef key, auto &&...v) {
+    o << '"' << key << '"' << ": [";
+    array_elements(v...);
+    o << "]";
+  }
+
+  void string(StringRef key, StringRef value, bool last = false) {
+    o << '"' << key << '"' << ": " << '"' << value << '"';
+    if (!last)
+      o << ",";
+  }
+};
+void gen(StringRef path, function_ref<void(dict &&)> fn) {
+  SmallString<256> file{};
+  sys::path::append(file, path, "Contents.json");
+
+  std::error_code ec;
+  auto o = raw_fd_stream(file, ec);
+
+  o << "{";
+  fn(dict{o});
+  o << "}";
+}
+} // namespace json
+
+static void create_xca_contents(StringRef xca) {
+  json::gen(xca, [](auto &&) {});
+}
+
+static void create_icon_contents(StringRef xca) {
+  json::gen(xca, [](auto &&d) {
+    d.array("images", [](auto &&dd) {
+      dd.string("filename", "icon.png");
+      dd.string("idiom", "universal");
+      dd.string("platform", "ios");
+      dd.string("size", "1024x1024", true);
+    });
+  });
+}
 
 bool actool(StringRef path) {
   auto app_path = sys::path::parent_path(path);
@@ -17,6 +77,14 @@ bool actool(StringRef path) {
   sys::path::append(plist, build_path, "icon-partial.plist");
   SmallString<256> xcassets{};
   sys::path::append(xcassets, build_path, "Assets.xcassets");
+  SmallString<256> appiconset{};
+  sys::path::append(appiconset, xcassets, "AppIcon.appiconset");
+
+  sys::fs::create_directories(xcassets);
+  sys::fs::create_directories(appiconset);
+
+  create_xca_contents(xcassets);
+  create_icon_contents(appiconset);
 
   auto cmd = ("actool "
               "--notices --warnings --errors "
