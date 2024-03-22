f656f5c334ed8cfa24786e312c47e9027aa49297
Eduardo Costa <m4c0@users.noreply.github.com>
2024-03-22 12:39:04 +0000

    log: colours

--- log.hpp
+++ log.hpp
@@ -1,8 +1,22 @@
 #pragma once
 #include <stdio.h>
 
+#ifndef _WIN32
+#include <unistd.h>
+static const bool is_tty = isatty(fileno(stderr));
+#else
+#include <io.h>
+static const bool is_tty = _isatty(_fileno(stderr));
+#endif
+
 static void vlog(const char *verb, const char *msg, int len) {
-  if (is_verbose())
+  if (!is_verbose())
+    return;
+
+  if (is_tty) {
+    fprintf(stderr, "\e[0;1m%20s\e[0m %.*s\n", verb, len, msg);
+  } else {
     fprintf(stderr, "%20s %.*s\n", verb, len, msg);
+  }
 }
 
