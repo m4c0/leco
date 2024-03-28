0e634e406ba322c902edfe48454d73731c4a4d7a
Eduardo Costa <m4c0@users.noreply.github.com>
2024-03-28 08:20:31 +0000

    in2out: preparing a sim_sb version

--- in2out.hpp
+++ in2out.hpp
@@ -1,8 +1,20 @@
 #pragma once
 #include "context.hpp"
+#include "sim.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
+
+static void in2out(const sim_sb *in, sim_sb *out, const char *ext) {
+  sim_sb_copy(out, in->buffer);
+  if (strcmp(in->buffer, "/out/") == 0) { // TODO: backslash on windows?
+    sim_sb_path_parent(out);
+    sim_sb_path_append(out, "out");
+    sim_sb_path_append(out, cur_ctx().target.c_str());
+    sim_sb_path_append(out, sim_sb_path_filename(in));
+  }
+  sim_sb_path_set_extension(out, ext);
+}
 
 static void in2out(llvm::StringRef in, llvm::SmallVectorImpl<char> &out,
                    llvm::StringRef ext) {
