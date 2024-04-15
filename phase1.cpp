0e3e5acc8924da4b4b043a134a6d4a7558312189
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-15 17:41:50 +0100

    phase1: remove cleaner

--- phase1.cpp
+++ phase1.cpp
@@ -1,9 +1,12 @@
+#include "cleaner.hpp"
 #include "dag.hpp"
 #include "sim.h"
 
 bool actool(const char *path) { return false; }
 void gen_iphone_plists(const char *exe_path, const char *name) {}
 bool find_android_llvm(sim_sb *out) { return false; }
+
+void clean(const dag::node *) {}
 
 void dag::node::write_to_cache_file() const {}
 bool dag::node::read_from_cache_file() { return false; }
