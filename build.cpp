18d961afacc40b536417c0e2d9a580a25c569d43
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-15 12:36:47 +0100

    build: using minirent instead of a manual file list

--- build.cpp
+++ build.cpp
@@ -1,3 +1,5 @@
+#define MINIRENT_IMPLEMENTATION
+#include "../minirent/minirent.h"
 #define MTIME_IMPLEMENTATION
 #include "../mtime/mtime.h"
 #define MKDIR_IMPLEMENTATION
@@ -5,47 +7,55 @@
 #define SIM_IMPLEMENTATION
 #include "sim.hpp"
 
-static constexpr const char *files[]{
-    "actool",  "bouncer", "cl",     "cleaner",    "compile",
-    "context", "dag",     "dag_io", "dag_plugin", "droid_path",
-    "evoker",  "impls",   "link",   "plist",      "target_defs",
-};
-
-bool compile(const char *stem) {
-  sim_sbt in{32};
+int compile(const char *in) {
   sim_sbt out{32};
-  sim_sb_printf(&in, "%s.cpp", stem);
-  sim_sb_printf(&out, "out/%s.o", stem);
+  sim_sb_printf(&out, "out/%s.o", in);
 
-  if (mtime_of(in.buffer) < mtime_of(out.buffer))
-    return true;
+  if (mtime_of(in) < mtime_of(out.buffer))
+    return 0;
 
-  fprintf(stderr, "compiling %s\n", stem);
+  fprintf(stderr, "compiling %s\n", in);
 
   sim_sbt buf{1024};
-  sim_sb_printf(&buf, "clang++ -std=c++20 -g -c %s -o %s", in.buffer,
-                out.buffer);
-  return 0 == system(buf.buffer);
+  sim_sb_printf(&buf, "clang++ -std=c++20 -g -c %s -o %s", in, out.buffer);
+  return system(buf.buffer);
 }
-bool link(const char *outf) {
-  sim_sbt buf{10240};
-  sim_sb_copy(&buf, "clang++ ");
-  for (auto f : files) {
-    sim_sb_printf(&buf, " out/%s.o ", f);
-  }
-  sim_sb_concat(&buf, outf);
-  return 0 == system(buf.buffer);
+
+static int error() {
+  fprintf(stderr, "Could not list current directory: %s\n", strerror(errno));
+  return 1;
 }
 
 int main(int argc, char **argv) {
   mkdirs("out");
-  for (auto f : files) {
-    if (!compile(f))
+
+  // should we make it work if running in a different dir?
+  DIR *dir = opendir(".");
+  if (dir == nullptr)
+    return error();
+
+  sim_sbt link{10240};
+  sim_sb_copy(&link, "clang++ -o leco.exe ");
+
+  dirent *ent{};
+  while ((ent = readdir(dir))) {
+    if (0 == strcmp("build.cpp", ent->d_name))
+      continue;
+
+    auto ext = sim_path_extension(ent->d_name);
+    if (ext == nullptr)
+      continue;
+
+    if (0 != strcmp(".cpp", ext))
+      continue;
+
+    if (0 != compile(ent->d_name))
       return 1;
+
+    sim_sb_printf(&link, " out/%s.o", ent->d_name);
   }
-  if (!compile("leco"))
-    return 1;
-  if (!link("out/leco.o -o leco.exe"))
-    return 1;
-  return 0;
+  closedir(dir);
+
+  fprintf(stderr, "linking leco.exe\n");
+  return system(link.buffer);
 }
