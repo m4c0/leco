4f54b1f35afd81a88fdaf802c19a63cdd2e8a59b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-15 14:58:13 +0100

    build: call phase 1
    Now, build creates phase 1 and then phase 1 creates final LECO.
    
    Eventually, phase1 can be lighter

--- build.cpp
+++ build.cpp
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  return system("clang++ -std=c++2b "
-                "bouncer.cpp cl.cpp cleaner.cpp compile.cpp context.cpp "
-                "dag.cpp dag_io.cpp dag_plugin.cpp evoker.cpp "
-                "leco.cpp link.cpp impls.cpp target_defs.cpp "
-                "phase1.cpp -o phase1.exe");
+  if (0 != system("clang++ -std=c++2b "
+                  "bouncer.cpp cl.cpp cleaner.cpp compile.cpp context.cpp "
+                  "dag.cpp dag_io.cpp dag_plugin.cpp evoker.cpp "
+                  "leco.cpp link.cpp impls.cpp target_defs.cpp "
+                  "phase1.cpp -o phase1.exe"))
+    return 1;
+
+  // TODO: make phase1 copy the final leco as its sibling
+  return system("./phase1.exe");
 }
