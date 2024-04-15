4862b50734493987c7a7cc22ba6b46a0edce94bb
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-15 15:07:19 +0100

    phase1: remove dag_io

--- build.cpp
+++ build.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
   if (0 != system("clang++ -std=c++2b "
                   "bouncer.cpp cl.cpp cleaner.cpp compile.cpp context.cpp "
-                  "dag.cpp dag_io.cpp dag_plugin.cpp evoker.cpp "
+                  "dag.cpp dag_plugin.cpp evoker.cpp "
                   "leco.cpp link.cpp impls.cpp target_defs.cpp "
                   "phase1.cpp -o phase1.exe"))
     return 1;
--- phase1.cpp
+++ phase1.cpp
@@ -1,5 +1,9 @@
+#include "dag.hpp"
 #include "sim.h"
 
 bool actool(const char *path) { return false; }
 void gen_iphone_plists(const char *exe_path, const char *name) {}
 bool find_android_llvm(sim_sb *out) { return false; }
+
+void dag::node::write_to_cache_file() const {}
+bool dag::node::read_from_cache_file() { return false; }
