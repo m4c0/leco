71c2970c5e00346e96c9f5797b493b4bfd6f464e
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-24 10:55:46 +0100

    clang: ignore unknown pragmas

--- clang.cpp
+++ clang.cpp
@@ -50,7 +50,7 @@ int main(int argc, char **argv) {
     clang_cmd(&args, "clang");
     sim_sb_concat(&args, " -std=c11");
   }
-  sim_sb_concat(&args, " -Wall");
+  sim_sb_concat(&args, " -Wall -Wno-unknown-pragmas");
 
   for (auto i = 0; i < opts.argc; i++) {
     // TODO: escape argv
