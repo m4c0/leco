471e87e45ebca481a6c39d9135a1d84827e09c61
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-01 10:54:27 +0100

    clang: support for targets and their defs

--- clang.cpp
+++ clang.cpp
@@ -4,6 +4,8 @@
 #define SIM_IMPLEMENTATION
 #include "sim.h"
 
+#include "host_target.hpp"
+
 static void clang_cmd(sim_sb *buf, const char *exe) {
 #if __APPLE__
   sim_sb_copy(buf, "/usr/local/opt/llvm@16/bin");
@@ -21,13 +23,48 @@ int usage() {
   return 1;
 }
 
+bool add_target_defs(sim_sb *buf, const char *tgt) {
+  if (0 == strcmp(tgt, "x86_64-pc-windows-msvc")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_WINDOWS");
+  } else if (0 == strcmp(tgt, "x86_64-pc-linux-gnu")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_LINUX");
+  } else if (0 == strcmp(tgt, "x86_64-apple-macosx11.6.0")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_MACOSX");
+    sim_sb_concat(buf, " -DLECO_TARGET_APPLE");
+    sim_sb_concat(buf, " -D_C99_SOURCE");
+  } else if (0 == strcmp(tgt, "arm64-apple-ios16.1")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_IPHONEOS");
+    sim_sb_concat(buf, " -DLECO_TARGET_IOS");
+    sim_sb_concat(buf, " -DLECO_TARGET_APPLE");
+    sim_sb_concat(buf, " -D_C99_SOURCE");
+  } else if (0 == strcmp(tgt, "x86_64-apple-ios16.1-simulator")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_IPHONESIMULATOR");
+    sim_sb_concat(buf, " -DLECO_TARGET_IOS");
+    sim_sb_concat(buf, " -DLECO_TARGET_APPLE");
+    sim_sb_concat(buf, " -D_C99_SOURCE");
+  } else if (0 == strcmp(tgt, "aarch64-none-linux-android26")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_ANDROID");
+  } else if (0 == strcmp(tgt, "armv7-none-linux-androideabi26")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_ANDROID");
+  } else if (0 == strcmp(tgt, "i686-none-linux-android26")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_ANDROID");
+  } else if (0 == strcmp(tgt, "x86_64-none-linux-android26")) {
+    sim_sb_concat(buf, " -DLECO_TARGET_ANDROID");
+  } else {
+    return false;
+  }
+  return true;
+}
+
 int main(int argc, char **argv) {
   struct gopt opts;
-  GOPT(opts, argc, argv, "cCgO");
+  GOPT(opts, argc, argv, "cCgOt");
 
   bool debug{};
   bool opt{};
   bool cpp = true;
+  const char *target{HOST_TARGET};
+
   char *val{};
   char ch;
   while ((ch = gopt_parse(&opts, &val)) != 0) {
@@ -43,6 +80,9 @@ int main(int argc, char **argv) {
       break;
     case 'O':
       opt = true;
+      break;
+    case 't':
+      target = val;
       break;
     default:
       return usage();
@@ -66,6 +106,11 @@ int main(int argc, char **argv) {
   if (opt) {
     sim_sb_concat(&args, " -O3 -flto -fvisibility=hidden");
   }
+
+  sim_sb_concat(&args, " -target ");
+  sim_sb_concat(&args, target);
+  if (!add_target_defs(&args, target))
+    return usage();
 
   for (auto i = 0; i < opts.argc; i++) {
     // TODO: escape argv
