f47f24261001dbd893c235cd4ff3bff0d694c02f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-02 10:44:37 +0100

    dagger: adding target option

--- dagger.cpp
+++ dagger.cpp
@@ -8,12 +8,17 @@ int usage() { return 1; }
 
 int main(int argc, char **argv) {
   struct gopt opts;
-  GOPT(opts, argc, argv, "");
+  GOPT(opts, argc, argv, "t:");
+
+  char *target{};
 
   char *val{};
   char ch;
   while ((ch = gopt_parse(&opts, &val)) != 0) {
     switch (ch) {
+    case 't':
+      target = val;
+      break;
     default:
       return usage();
     }
