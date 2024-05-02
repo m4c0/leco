5eaaad5baa55ac277d3c7fe723818f3be9923867
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-02 17:16:14 +0100

    dagger: taking the exe type

--- dagger.cpp
+++ dagger.cpp
@@ -6,9 +6,32 @@
 #include "../popen/popen.h"
 #include "sim.hpp"
 
-int usage() {
+enum class exe_t {
+  none,
+  dll,
+  tool,
+  app,
+};
+
+static char *source{};
+static unsigned line{};
+static exe_t exe_type{};
+
+static int usage() {
   fprintf(stderr, "invalid usage\n");
   return 1;
+}
+
+static void error(const char *msg) {
+  fprintf(stderr, "%s:%d: %s\n", source, line, msg);
+  throw 0;
+}
+
+static char *cmp(char *str, const char *prefix) {
+  auto len = strlen(prefix);
+  if (strncmp(str, prefix, len) != 0)
+    return nullptr;
+  return str + len;
 }
 
 static void stamp(sim_sb *args, char **&argp, const char *arg) {
@@ -17,17 +40,27 @@ static void stamp(sim_sb *args, char **&argp, const char *arg) {
   sim_sb_concat(args, arg);
 }
 
+static void set_exe_type(exe_t t) {
+  if (exe_type != exe_t::none)
+    error("multiple executable type found");
+  exe_type = t;
+}
+
 int main(int argc, char **argv) {
   struct gopt opts;
-  GOPT(opts, argc, argv, "t:i:");
+  GOPT(opts, argc, argv, "t:i:d");
 
+  bool dump_errors{};
   char *target{};
-  char *source{};
+  // FILE *out = stdout;
 
   char *val{};
   char ch;
   while ((ch = gopt_parse(&opts, &val)) != 0) {
     switch (ch) {
+    case 'd':
+      dump_errors = true;
+      break;
     case 'i':
       source = val;
       break;
@@ -66,5 +99,32 @@ int main(int argc, char **argv) {
   FILE *ferr;
   if (0 != proc_open(clang_argv, &f, &ferr))
     return 1;
-}
+
+  line = 0;
+  sim_sbt mod_name{};
+
+  char buf[1024];
+  while (!feof(f) && fgets(buf, 1024, f) != nullptr) {
+    char *p = buf;
+    while (*p == ' ') {
+      p++;
+    }
+    line++;
+    if (0 == strcmp(p, "#pragma leco tool\n")) {
+      set_exe_type(exe_t::tool);
+    } else if (cmp(p, "#pragma leco app\n")) {
+      set_exe_type(exe_t::app);
+    } else if (cmp(p, "#pragma leco dll\n")) {
+      set_exe_type(exe_t::dll);
+    }
+  }
+
+  if (dump_errors) {
+    while (!feof(ferr) && fgets(buf, 1024, ferr) != nullptr) {
+      fputs(buf, stderr);
+    }
+  }
+
+  fclose(f);
+  fclose(ferr);
 }
