3a536a985f48d97d0c8eea411e4f08c9c3a0faf2
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-08 18:02:50 +0100

    deplist: refactoring for future recursion

--- deplist.cpp
+++ deplist.cpp
@@ -10,9 +10,12 @@
 #include <stdio.h>
 #include <string.h>
 
+static FILE *out{};
+static const char *target{};
+
 static void usage() { die("invalid usage"); }
 
-void print_dep(FILE *out, const char *file, const char *target) {
+static void print_dep(const char *file) {
   sim_sbt stem{};
   sim_sb_path_copy_stem(&stem, file);
 
@@ -30,23 +33,10 @@ void print_dep(FILE *out, const char *file, const char *target) {
   fprintf(out, "-fmodule-file=%s=%s\n", stem.buffer, pcm.buffer);
 }
 
-void read_dag(const char *dag) {
+static void read_dag(const char *dag) {
   FILE *f{};
   if (0 != fopen_s(&f, dag, "r"))
     die("dag file not found: [%s]\n", dag);
-
-  sim_sbt out{};
-  sim_sb_copy(&out, dag);
-  sim_sb_path_set_extension(&out, "deps");
-
-  FILE *o{};
-  if (0 != fopen_s(&o, out.buffer, "wb")) {
-    die("could not open output file: [%s]\n", out.buffer);
-  }
-
-  sim_sbt path{};
-  sim_sb_path_copy_parent(&path, out.buffer);
-  const char *target = sim_sb_path_filename(&path);
 
   char buf[10240];
   while (!feof(f) && fgets(buf, sizeof(buf), f) != nullptr) {
@@ -59,7 +49,7 @@ void read_dag(const char *dag) {
 
     switch (*id) {
     case 'mdep':
-      print_dep(o, file, target);
+      print_dep(file);
       break;
     default:
       break;
@@ -93,7 +83,21 @@ void run(int argc, char **argv) {
   if (!strstr(input, SIM_PATHSEP_S "out" SIM_PATHSEP_S))
     usage();
 
+  sim_sbt output{};
+  sim_sb_copy(&output, input);
+  sim_sb_path_set_extension(&output, "deps");
+
+  if (0 != fopen_s(&out, output.buffer, "wb")) {
+    die("could not open output file: [%s]\n", output.buffer);
+  }
+
+  sim_sbt path{};
+  sim_sb_path_copy_parent(&path, output.buffer);
+  target = sim_sb_path_filename(&path);
+
   read_dag(input);
+
+  fclose(out);
 }
 
 int main(int argc, char **argv) {
