12fa7174bd274c83316a44ddecc6970b15c91dc1
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-09 22:02:44 +0100

    link: using dag files to list args

--- leco-link.cpp
+++ leco-link.cpp
@@ -5,17 +5,71 @@
 #include "../gopt/gopt.h"
 #include "die.hpp"
 #include "fopen.hpp"
+#include "in2out.hpp"
 #include "sim.hpp"
 
-void usage() { die("invalid usage"); }
+#include <set>
+#include <string>
+
+static const char *target{};
+static FILE *out{};
+static std::set<std::string> added{};
+
+static void usage() { die("invalid usage"); }
+
+static void read_dag(const char *dag) {
+  auto [_, inserted] = added.insert(dag);
+  if (!inserted)
+    return;
+
+  sim_sbt obj{};
+  sim_sb_copy(&obj, dag);
+  sim_sb_path_set_extension(&obj, "o");
+  fprintf(out, "%s\n", obj.buffer);
+
+  FILE *f{};
+  if (0 != fopen_s(&f, dag, "r"))
+    die("dag file not found: [%s]\n", dag);
+
+  char buf[10240];
+  while (!feof(f) && fgets(buf, sizeof(buf), f) != nullptr) {
+    if (strlen(buf) < 5)
+      die("invalid line in dag file");
+
+    uint32_t *id = reinterpret_cast<uint32_t *>(buf);
+    char *file = reinterpret_cast<char *>(id + 1);
+    file[strlen(file) - 1] = 0;
+
+    switch (*id) {
+    case 'frwk':
+      fprintf(out, "-framework\n%s\n", file);
+      break;
+    case 'libr':
+      fprintf(out, "-l%s\n", file);
+      break;
+    case 'ldir':
+      fprintf(out, "-L%s\n", file);
+      break;
+    case 'impl':
+    case 'mdep': {
+      sim_sbt ddag{};
+      in2out(file, &ddag, "dag", target);
+      read_dag(ddag.buffer);
+      break;
+    }
+    default:
+      break;
+    }
+  }
+
+  fclose(f);
+}
 
 int main(int argc, char **argv) try {
   struct gopt opts;
-  GOPT(opts, argc, argv, "i:o:t:");
+  GOPT(opts, argc, argv, "i:");
 
   const char *input{};
-  const char *output{};
-  const char *target{};
 
   char *val{};
   char ch;
@@ -24,12 +78,6 @@ int main(int argc, char **argv) try {
     case 'i':
       input = val;
       break;
-    case 'o':
-      output = val;
-      break;
-    case 't':
-      target = val;
-      break;
     default:
       usage();
     }
@@ -37,20 +85,19 @@ int main(int argc, char **argv) try {
   if (opts.argc != 0)
     usage();
 
+  sim_sbt path{};
+  sim_sb_path_copy_parent(&path, input);
+  target = sim_sb_path_filename(&path);
+
   sim_sbt args{};
   sim_sb_copy(&args, input);
   sim_sb_path_set_extension(&args, "link");
 
-  FILE *out{};
   if (0 != fopen_s(&out, args.buffer, "wb")) {
     die("could not open argument file: [%s]\n", args.buffer);
   }
 
-#ifdef _WIN32 // otherwise, face LNK1107 errors from MSVC
-  fputs("-fuse-ld=lld", out);
-#endif
-  fputs("-o", out);
-  fputs(output, out);
+  read_dag(input);
 
   fclose(out);
   return 0;
