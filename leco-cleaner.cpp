09ac235ac45efd51840bc091774935eafe075325
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-13 18:29:18 +0100

    cleaner: initial no-op implementation

--- leco-cleaner.cpp
+++ leco-cleaner.cpp
@@ -1,10 +1,12 @@
 #pragma leco tool
 #define GOPT_IMPLEMENTATION
-#define MINIRENT_IMPLEMENTATION
+#define PPRENT_IMPLEMENTATION
 #define SIM_IMPLEMENTATION
 
 #include "../gopt/gopt.h"
-#include "../minirent/minirent.h"
+#include "../pprent/pprent.hpp"
+#include "die.hpp"
+#include "fopen.hpp"
 #include "host_target.hpp"
 #include "log.hpp"
 #include "sim.hpp"
@@ -29,28 +31,81 @@ where:
 }
 
 static void rm_rf(sim_sb *path) {
-  DIR *dir = opendir(path->buffer);
-  if (dir != nullptr) {
-    dirent *ds;
-    while ((ds = readdir(dir)) != nullptr) {
-      if (ds->d_name[0] == '.')
-        continue;
-      sim_sb_path_append(path, ds->d_name);
-      rm_rf(path);
-      sim_sb_path_parent(path);
-    }
-    closedir(dir);
+  for (auto entry : pprent::list(path->buffer)) {
+    if (entry[0] == '.')
+      continue;
+    sim_sb_path_append(path, entry);
+    rm_rf(path);
+    sim_sb_path_parent(path);
   }
   if (log_all)
-    vlog("removing", path->buffer);
-  unlink(path->buffer);
+    log("removing", path->buffer);
+  // unlink(path->buffer);
 }
 
-static void remove_with_deps(const char *path) {}
+static void rm_out(const char *parent) {
+  sim_sbt path{};
+  sim_sb_copy(&path, parent);
+  sim_sb_path_append(&path, "out");
+  sim_sb_path_append(&path, target);
+  if (!log_all)
+    log("removing", path.buffer);
+
+  rm_rf(&path);
+}
+
+#include <set>
+#include <string>
+static std::set<std::string> temp{};
+static void remove_with_deps(sim_sb *path) {
+  auto [_, x] = temp.emplace(path->buffer);
+  if (!x)
+    return;
+
+  sim_sb_path_append(path, "out");
+  sim_sb_path_append(path, target);
+
+  for (auto entry : pprent::list(path->buffer)) {
+    if (0 != strcmp(".dag", sim_path_extension(entry)))
+      continue;
+
+    sim_sb_path_append(path, entry);
+    FILE *f{};
+    if (0 != fopen_s(&f, path->buffer, "r"))
+      die("failed to remove: %s", path->buffer);
+
+    char buf[10240];
+    while (!feof(f) && fgets(buf, sizeof(buf), f) != nullptr) {
+      if (strlen(buf) < 5)
+        die("invalid line in dag file [%s]", path->buffer);
+
+      uint32_t *id = reinterpret_cast<uint32_t *>(buf);
+      char *file = reinterpret_cast<char *>(id + 1);
+      file[strlen(file) - 1] = 0;
+
+      switch (*id) {
+      case 'impl':
+      case 'mdep': {
+        sim_sbt p{};
+        sim_sb_path_copy_parent(&p, file);
+        remove_with_deps(&p);
+        break;
+      }
+      default:
+        break;
+      }
+    }
+
+    fclose(f);
+    sim_sb_path_parent(path);
+  }
+
+  rm_out(path->buffer);
+}
 
 int main(int argc, char **argv) try {
   struct gopt opts;
-  GOPT(opts, argc, argv, "av");
+  GOPT(opts, argc, argv, "avt:");
 
   bool all{};
 
@@ -75,9 +130,12 @@ int main(int argc, char **argv) try {
   if (opts.argc != 0)
     usage(argv[0]);
 
+  sim_sbt cwd{};
+  sim_sb_path_copy_real(&cwd, ".");
   if (all) {
-    remove_with_deps();
+    remove_with_deps(&cwd);
   } else {
+    rm_out(cwd.buffer);
   }
 
   return 0;
