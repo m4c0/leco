322fdfe1ce66337cb03ee16ef9bf5637c2fa8832
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-18 11:41:42 +0100

    phase1: fix build

--- phase1.cpp
+++ phase1.cpp
@@ -7,6 +7,8 @@ bool actool(const char *path) { return false; }
 void gen_iphone_plists(const char *exe_path, const char *name) {}
 bool find_android_llvm(sim_sb *out) { return false; }
 
+bool should_clean_current() { return false; }
+bool should_clean_all() { return false; }
 bool is_verbose() { return true; }
 bool is_extra_verbose() { return false; }
 bool is_optimised() { return true; }
