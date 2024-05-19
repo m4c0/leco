6572f423d5def4997dd70d332a94d7aabff93698
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-19 13:07:17 +0100

    fopen: rule of 5

--- fopen.hpp
+++ fopen.hpp
@@ -5,9 +5,10 @@
 
 namespace f {
 class open {
-  FILE *m_f;
+  FILE *m_f{};
 
 public:
+  constexpr open() = default;
   open(const char *name, const char *mode) {
 #ifdef _WIN32
     if (0 != fopen_s(&m_f, name, mode))
@@ -18,7 +19,21 @@ public:
       die("could not open file [%s]", name);
 #endif
   }
-  ~open() { fclose(m_f); }
+  ~open() {
+    if (m_f)
+      fclose(m_f);
+  }
+
+  open(const open &o) = delete;
+  open &operator=(const open &o) = delete;
+  open(open &&o) : m_f{o.m_f} { o.m_f = nullptr; }
+  open &operator=(open &&o) {
+    if (m_f)
+      fclose(m_f);
+    m_f = o.m_f;
+    o.m_f = nullptr;
+    return *this;
+  }
 
   constexpr FILE *operator*() { return m_f; }
 };
