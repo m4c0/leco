3f1cb49429efc919e2d6b17089ac29e6b3ba256c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-22 19:38:59 +0100

    exs: more boilerplate

--- leco-exs.cpp
+++ leco-exs.cpp
@@ -1,11 +1,41 @@
 #pragma leco tool
 
 #define GOPT_IMPLEMENTATION
+#define SIM_IMPLEMENTATION
 
+#include "dag2.hpp"
 #include "die.hpp"
 #include "gopt.hpp"
+#include "in2out.hpp"
+#include "sim.hpp"
+
+#include <set>
+#include <string>
+
+static const char *target;
 
 static void usage() { die("invalid usage"); }
+
+static std::set<std::string> added{};
+static void read_dag(const char *dag) {
+  auto [_, inserted] = added.insert(dag);
+  if (!inserted)
+    return;
+
+  dag_read(dag, [](auto id, auto file) {
+    switch (id) {
+    case 'impl':
+    case 'mdep': {
+      sim_sbt ddag{};
+      in2out(file, &ddag, "dag", target);
+      read_dag(ddag.buffer);
+      break;
+    }
+    default:
+      break;
+    }
+  });
+}
 
 int main(int argc, char **argv) try {
   const char *input{};
@@ -28,6 +58,12 @@ int main(int argc, char **argv) try {
     usage();
   if (opts.argc != 0)
     usage();
+
+  sim_sbt path{};
+  sim_sb_path_copy_parent(&path, input);
+  target = sim_sb_path_filename(&path);
+
+  read_dag(input);
 } catch (...) {
   return 1;
 }
