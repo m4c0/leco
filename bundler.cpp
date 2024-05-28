3cb218ebc491c3317073f669342fc533a1ca9d53
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-28 10:23:13 +0100

    bundler: merging "in2exe" logic

--- bundler.cpp
+++ bundler.cpp
@@ -11,21 +11,6 @@
 
 void prep(sim_sb *cmd, const char *tool);
 
-static void in2exe(const dag::node *n, sim_sb *out) {
-  in2out(n->source(), out, "exe", cur_ctx().target.c_str());
-
-  if (n->app()) {
-    sim_sbt stem{};
-    sim_sb_path_copy_sb_stem(&stem, out);
-
-    cur_ctx().app_exe_path(out, stem.buffer);
-
-    sim_sbt path{};
-    sim_sb_path_copy_parent(&path, out->buffer);
-    mkdirs(path.buffer);
-  }
-}
-
 static void copy(const char *with, const dag::node *n, const char *to) {
   sim_sbt cmd{};
   prep(&cmd, with);
@@ -41,14 +26,21 @@ void bundle(const dag::node *n) {
   sim_sb_copy(&exe, n->dag());
   sim_sb_path_set_extension(&exe, "exe");
 
+  sim_sbt stem{};
+  sim_sb_path_copy_sb_stem(&stem, &exe);
+
   sim_sbt res_path{};
   sim_sb_copy(&res_path, exe.buffer);
   cur_ctx().app_res_path(&res_path);
   mkdirs(res_path.buffer);
 
   sim_sbt exe_path{};
-  in2exe(n, &exe_path);
-  sim_sb_path_parent(&exe_path);
+  in2out(n->source(), &exe_path, "exe", cur_ctx().target.c_str());
+  cur_ctx().app_exe_path(&exe_path, stem.buffer);
+
+  sim_sbt path{};
+  sim_sb_path_copy_parent(&path, exe_path.buffer);
+  mkdirs(exe_path.buffer);
 
   copy("leco-exs.exe", n, exe_path.buffer);
   copy("leco-rsrc.exe", n, res_path.buffer);
