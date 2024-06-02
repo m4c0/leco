16a2ac8765402cfead0634533bbe179326d691e8
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-02 11:00:46 +0100

    removing dead code

--- leco-driver.cpp
+++ leco-driver.cpp
@@ -3,7 +3,6 @@
 #pragma leco add_impl cl
 #pragma leco add_impl dag
 #pragma leco add_impl dag_plugin
-#pragma leco add_impl droid_path
 #pragma leco add_impl impls
 #pragma leco add_impl target_defs
 
