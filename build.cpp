2307d5e1f1554c8104b680cf1bf93056491271d4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-05 10:19:38 +0100

    build: using leco-clang for leco.exe

--- build.cpp
+++ build.cpp
@@ -25,11 +25,11 @@ int try_main(int argc, char **argv) {
   // TODO: self-rebuild "phase 0" (aka "this cpp")
   mkdirs("out" SEP HOST_TARGET);
 
-  puts("Building meta runner");
-  run("clang++ -Wall -Wno-unknown-pragmas -std=c++20 leco.cpp -o leco.exe");
-
   puts("Building clang runner");
   run("clang++ -Wall -Wno-unknown-pragmas -std=c++20 leco-clang.cpp -o " CLANG);
+
+  puts("Building meta runner");
+  run(CLANG " -i leco.cpp -o leco.exe");
 
   puts("Building deplist");
   run(CLANG " -i leco-deplist.cpp -o out/" HOST_TARGET "/leco-deplist.exe");
