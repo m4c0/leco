e0a60ffc5c38e7c580b74e3361953745d94c6869
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-12 11:29:44 +0100

    driver: removing usage of old bits

--- leco-driver.cpp
+++ leco-driver.cpp
@@ -5,8 +5,6 @@
 #define GOPT_IMPLEMENTATION
 #define PPRENT_IMPLEMENTATION
 #define SIM_IMPLEMENTATION
-#include "cl.hpp"
-#include "context.hpp"
 #include "die.hpp"
 #include "fopen.hpp"
 #include "gopt.hpp"
@@ -18,9 +16,18 @@
 #include <errno.h>
 #include <string.h>
 
-const char *leco_argv0;
+#ifdef _WIN32
+#include <direct.h>
+#define chdir _chdir
+#else
+#include <unistd.h>
+#endif
 
-void usage() {
+const char *leco_argv0;
+const char *common_flags;
+unsigned clean_level{};
+
+static void usage() {
   fprintf(stderr, R"(
   Usage: ../leco/leco.exe [-C <dir>] [-D] [-g] [-O] [-t <target>] [-v]
 
@@ -43,30 +50,32 @@ void usage() {
   throw 0;
 }
 
-void prep(sim_sb *cmd, const char *tool) {
+static void prep(sim_sb *cmd, const char *tool) {
   sim_sb_path_copy_parent(cmd, leco_argv0);
   sim_sb_path_append(cmd, tool);
 }
 
 static bool error() {
   perror("could not list current directory");
-  return false;
+  throw 0;
 }
 
-static void cleaner() {
-  if (!should_clean_current())
+static void cleaner(const char *target) {
+  if (clean_level == 0)
     return;
 
   sim_sbt cmd{};
   prep(&cmd, "leco-cleaner.exe");
-  if (should_clean_all()) {
+  sim_sb_concat(&cmd, " -t ");
+  sim_sb_concat(&cmd, target);
+  if (clean_level > 1) {
     sim_sb_concat(&cmd, " -a");
   }
   run(cmd.buffer);
 }
 
-bool run_target() {
-  cleaner();
+static void run_target(const char *target) {
+  cleaner(target);
 
   for (auto file : pprent::list(".")) {
     auto ext = sim_path_extension(file);
@@ -79,33 +88,107 @@ bool run_target() {
     sim_sbt cmd{};
     prep(&cmd, "leco-recurse.exe");
     sim_sb_concat(&cmd, " -t ");
-    sim_sb_concat(&cmd, cur_ctx().target.c_str());
+    sim_sb_concat(&cmd, target);
     sim_sb_concat(&cmd, " -i ");
     sim_sb_concat(&cmd, file);
-    if (is_optimised())
-      sim_sb_concat(&cmd, " -g");
-    if (is_optimised())
-      sim_sb_concat(&cmd, " -O");
+    sim_sb_concat(&cmd, common_flags);
     run(cmd.buffer);
 
     errno = 0;
   }
-  return errno ? error() : true;
+  if (errno)
+    error();
+}
+
+static void run_targets(const char *target) {
+#ifdef __APPLE__
+  if (0 == strcmp(target, "apple")) {
+    run_target(TGT_OSX);
+    run_target(TGT_IPHONEOS);
+    run_target(TGT_IOS_SIMULATOR);
+    return;
+  }
+
+  if (0 == strcmp(target, "ios")) {
+    run_target(TGT_IPHONEOS);
+    run_target(TGT_IOS_SIMULATOR);
+    return;
+  }
+
+  if (0 == strcmp(target, "host") || 0 == strcmp(target, "macosx")) {
+    run_target(TGT_OSX);
+    return;
+  }
+  if (0 == strcmp(target, "iphoneos")) {
+    run_target(TGT_IPHONEOS);
+    return;
+  }
+  if (0 == strcmp(target, "iphonesimulator")) {
+    run_target(TGT_IOS_SIMULATOR);
+    return;
+  }
+#endif
+
+#ifdef _WIN32
+  if (0 == strcmp(target, "host") || 0 == strcmp(target, "windows")) {
+    run_target(TGT_WINDOWS);
+    return;
+  }
+#endif
+
+#ifdef __linux__
+  if (0 == strcmp(target, "host") || 0 == strcmp(target, "linux")) {
+    run_target(TGT_LINUX);
+    return;
+  }
+#endif
+
+  if (0 == strcmp(target, "ios")) {
+    run_target(TGT_DROID_AARCH64);
+    run_target(TGT_DROID_ARMV7);
+    run_target(TGT_DROID_X86);
+    run_target(TGT_DROID_X86_64);
+    return;
+  }
+
+  die("unknown target: %s", target);
 }
 
 extern "C" int main(int argc, char **argv) try {
   leco_argv0 = argv[0];
 
-  if (!parse_args(argc, argv))
+  const char *target{"host"};
+  sim_sbt flags{};
+  auto opts = gopt_parse(argc, argv, "C:cgOt:", [&](auto ch, auto val) {
+    switch (ch) {
+    case 'c':
+      clean_level++;
+      break;
+    case 'C':
+      if (0 != chdir(val)) {
+        die("Directory not found: [%s]\n", val);
+      }
+      break;
+    case 'g':
+      sim_sb_concat(&flags, " -g");
+      break;
+    case 'O':
+      sim_sb_concat(&flags, " -O");
+      break;
+    case 't':
+      target = val;
+      break;
+    default:
+      usage();
+      break;
+    }
+  });
+  if (opts.argc != 0)
     usage();
 
-  if (!for_each_target(run_target))
-    return 1;
-
+  common_flags = flags.buffer;
+  run_targets(target);
   return 0;
-} catch (const std::exception &e) {
-  fprintf(stderr, "exception: %s\n", e.what());
-  return 1;
 } catch (...) {
   return 1;
 }
