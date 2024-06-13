16186db2f2a6210d72c73377796feeb0fc676747
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-13 07:30:10 +0100

    log: cleaning up

--- log.hpp
+++ log.hpp
@@ -1,28 +1,6 @@
 #pragma once
 #include <stdio.h>
 
-#ifndef _WIN32
-#include <unistd.h>
-static const bool is_tty = isatty(fileno(stderr));
-#else
-#include <io.h>
-static const bool is_tty = _isatty(_fileno(stderr));
-#endif
-
-// NOTE: always log to stderr to avoid order issues
-static void log(unsigned colour, const char *verb, const char *msg) {
-  if (is_tty) {
-    fprintf(stderr, "\e[1;%dm%20s\e[0m %s\n", colour, verb, msg);
-  } else {
-    fprintf(stderr, "%20s %s\n", verb, msg);
-  }
-}
 static inline void log(const char *verb, const char *msg) {
-  log(34, verb, msg);
-}
-static inline void wlog(const char *verb, const char *msg) {
-  log(33, verb, msg);
-}
-static inline void dlog(const char *verb, const char *msg) {
-  log(35, verb, msg);
+  fprintf(stderr, "%20s %s\n", verb, msg);
 }
