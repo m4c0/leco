79d18ad9b926f5b82df49318303998353f7b1ace
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-24 10:18:50 +0100

    format: making a more sensible approach
    let's default to format files with a default parameter set until proven
    otherwise

--- leco-format.cpp
+++ leco-format.cpp
@@ -2,7 +2,7 @@
 #define SIM_IMPLEMENTATION
 #include "sim.hpp"
 
-import gopt;
+import mtime;
 import sys;
 
 static const char *fmt_cmd() {
@@ -15,28 +15,19 @@ static const char *fmt_cmd() {
 #endif
 }
 
-static void usage() { sys::die("invalid usage"); }
-
 int main(int argc, char **argv) try {
-  const char *input{};
-
-  // TODO: add an embedded config to be used if ".clang-format" does not exist
-  auto opts = gopt_parse(argc, argv, "i:", [&](auto ch, auto val) {
-    switch (ch) {
-    case 'i':
-      input = val;
-      break;
-    default:
-      usage();
-    }
-  });
-
-  // TODO: default to format all files in current dir
-  if (!input || opts.argc != 0)
-    usage();
-
   sim_sbt cmd{10240};
-  sim_sb_printf(&cmd, "%s -i %s", fmt_cmd(), input);
+  sim_sb_copy(&cmd, fmt_cmd());
+  sim_sb_concat(&cmd, " -i");
+
+  for (auto i = 1; i < argc; i++) {
+    if (mtime::of(argv[i]) == 0) {
+      sys::die("file not found: %s", argv[i]);
+    }
+
+    sim_sb_printf(&cmd, " %s", argv[i]);
+  }
+
   sys::run(cmd.buffer);
 } catch (...) {
   return 1;
