225b3f6eab2e060ac12255a074672cc8dec4f965
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-02 10:39:20 +0100

    clang: removing the llvm version in osx
    not entirely sure why this was needed and I'm now too lazy to dig into
    git history. It's easy to rollback, so next time I add a comment to
    remember in an easier way.

--- leco-clang.cpp
+++ leco-clang.cpp
@@ -16,7 +16,7 @@ static const char *argv0;
 
 static void clang_cmd(sim_sb *buf, const char *exe) {
 #if __APPLE__
-  sim_sb_copy(buf, "/usr/local/opt/llvm@16/bin");
+  sim_sb_copy(buf, "/usr/local/opt/llvm/bin");
   sim_sb_path_append(buf, exe);
 #elif _WIN32
   sim_sb_copy(buf, exe);
