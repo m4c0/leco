bc7da1b0c610784bf9b5ec9550e3c3855234c420
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-06 10:47:07 +0100

    meta: removing TODOs done

--- leco.cpp
+++ leco.cpp
@@ -13,8 +13,6 @@
 
 #include <stdlib.h>
 
-// TODO: leco-format calling clang-format
-// TODO: leco driver calling format after a successful compilation
 // TODO: add clang-format config to leco-format
 
 import gopt;
