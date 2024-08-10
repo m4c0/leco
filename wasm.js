4ab52e46d3f9b72d918e3a5b17f22ac36d0fd55d
Eduardo <m4c0@users.noreply.github.com>
2024-08-10 19:33:16 +0100

    wasm: remove "wasi preview" from imports
    this lets vaselin take care of it

--- wasm.js
+++ wasm.js
@@ -1,10 +1,8 @@
 (function() {
-  const wasi_snapshot_preview1 = {};
-  const imp = { wasi_snapshot_preview1, ...leco_imports };
   const loc = location.pathname.replace(/^.*[/]/, "").replace(/[.].*$/, "");
   fetch(loc + ".wasm")
     .then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes, imp))
+    .then(bytes => WebAssembly.instantiate(bytes, leco_imports))
     .then(obj => {
       leco_exports = obj.instance.exports;
       obj.instance.exports._initialize();
