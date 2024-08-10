0498407c9b803626486de8bc97371b236bb91b75
Eduardo <m4c0@users.noreply.github.com>
2024-08-10 11:16:45 +0100

    webserver: run a build before the server

--- leco-webserver.cpp
+++ leco-webserver.cpp
@@ -1,5 +1,20 @@
 #pragma leco tool
+#define SIM_IMPLEMENTATION
+#include "sim.hpp"
 
 import sys;
 
-int main(int argc, char ** argv) { sys::run("python3 -mhttp.server"); }
+int main(int argc, char ** argv) {
+  sim_sbt cmd { 10240 };
+  sim_sb_path_copy_real(&cmd, ".");
+  sim_sb_path_parent(&cmd);
+  sim_sb_path_append(&cmd, "leco");
+  sim_sb_path_append(&cmd, "leco.exe");
+
+  for (auto i = 1; i < argc; i++) {
+    sim_sb_printf(&cmd, " %s", argv[i]);
+  }
+
+  sys::run(cmd.buffer);
+  sys::run("python3 -mhttp.server");
+}
