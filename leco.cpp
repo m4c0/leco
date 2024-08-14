3f9179835c03ffafece48f000afef708c8023e02
Eduardo <m4c0@users.noreply.github.com>
2024-08-14 19:39:09 +0100

    leco: using SIM because windows sucks
    exec calls on Windows use "CreateProcess" so they don't actually replace
    the parent process. And they also "detach", so - never use it.

--- leco.cpp
+++ leco.cpp
@@ -1,20 +1,18 @@
-#include <string.h>
+#define SIM_IMPLEMENTATION
 
-#if _WIN32
-#include <process.h>
-#define execvp _execvp
-#define strdup _strdup
-#define SEP "\\"
-#else
-#include <unistd.h>
-#define SEP "/"
-#endif
-
+#include "sim.h"
 #include "targets.hpp"
 
+#define SEP SIM_PATHSEP_S
 #define CMD ".." SEP "leco" SEP "out" SEP HOST_TARGET SEP "leco-meta.exe"
 
 int main(int argc, char ** argv) {
-  argv[0] = strdup(CMD);
-  return execvp(CMD, argv);
+  sim_sb cmd {};
+  sim_sb_new(&cmd, 10240);
+  sim_sb_copy(&cmd, CMD);
+  for (auto i = 1; i < argc; i++) {
+    // TODO: escape
+    sim_sb_printf(&cmd, " %s", argv[i]);
+  }
+  return system(cmd.buffer);
 }
