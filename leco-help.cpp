ccf5752431acc8a1172ed1d81a9e0c6fe18374bd
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-22 10:41:33 +0100

    help: adding newer tools

--- leco-help.cpp
+++ leco-help.cpp
@@ -9,6 +9,8 @@ Existing tools:
 
         cleaner: cleans output folders
         driver: drives the LECO workflow (default tool if unspecified)
+        format: formats code using clang-format
+        git-std: applies some "standard options" to a git repo
         git-status: list status of all folders in the git workspace
         git-update: recursively updates known dependencies
         help: this tool
@@ -23,6 +25,7 @@ Low-level plumbing tools:
         ipa: generates and signs IPAs
         link: links an executable
         recurse: compiles C++ code recursively
+        sawblade: runs dagger recursively
         sysroot: calculates the sysroot of the environment
         xcassets: generates xcassets for IPAs
 
