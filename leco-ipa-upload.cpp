01e3a5bd658fb1a9baf95e1eb375ea0cb0f22a32
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-12 07:45:14 +0100

    ipa-upload: flag to switch validate/upload

--- leco-ipa-upload.cpp
+++ leco-ipa-upload.cpp
@@ -18,16 +18,22 @@ Must be run from the root of the source repository.
 It only support iPhoneOS exports. No support for other Apple OSs like WatchOS,
 etc.
 
-Usage: ../leco/leco.exe ipa-upload -i <input-dag>
+Usage: ../leco/leco.exe ipa-upload -i <input-dag> [-s]
+
+Where:
+        -i: input dag
+        -s: submit (defaults to validate)
 
 )");
 }
 
 int main(int argc, char ** argv) try {
   const char * input {};
-  auto opts = gopt_parse(argc, argv, "i:", [&](auto ch, auto val) {
+  bool upload {};
+  auto opts = gopt_parse(argc, argv, "i:s", [&](auto ch, auto val) {
     switch (ch) {
       case 'i': input = val; break;
+      case 's': upload = true; break;
       default: usage();
     }
   });
@@ -42,10 +48,12 @@ int main(int argc, char ** argv) try {
   sim_sb_path_append(&ipa, sim_path_filename(input));
   sim_sb_path_set_extension(&ipa, "ipa");
 
+  const char * verb = upload ? "--upload-app" : "--validate-app";
+
   sim_sbt cmd { 10240 };
   sim_sb_printf(&cmd, 
-      "xcrun altool --validate-app -t iphoneos -f %s --apiKey %s --apiIssuer %s",
-      ipa.buffer, sys::env("LECO_IOS_API_KEY"), sys::env("LECO_IOS_API_ISSUER"));
+      "xcrun altool %s -t iphoneos -f %s --apiKey %s --apiIssuer %s",
+      verb, ipa.buffer, sys::env("LECO_IOS_API_KEY"), sys::env("LECO_IOS_API_ISSUER"));
   sys::run(cmd.buffer);
 } catch (...) {
   return 1;
