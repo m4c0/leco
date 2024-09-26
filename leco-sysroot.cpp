6155b4ccb1ad0c0294aa8bfc94c8ffe7451f6576
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-26 11:13:46 +0100

    sysroot: a bit of reformatting

--- leco-sysroot.cpp
+++ leco-sysroot.cpp
@@ -19,8 +19,7 @@ static void find_android_llvm(sim_sb *res) {
   const auto sdk = sys::env("ANDROID_SDK_ROOT");
 
   sim_sb_path_copy_append(res, sdk, "ndk-bundle");
-  if (exists(res))
-    return;
+  if (exists(res)) return;
 
   sim_sb_path_copy_append(res, sdk, "ndk");
   if (!exists(res))
@@ -31,8 +30,7 @@ static void find_android_llvm(sim_sb *res) {
   sim_sb_copy(&max, "");
 
   for (auto e : pprent::list(res->buffer)) {
-    if (strcmp(max.buffer, e) > 0)
-      continue;
+    if (strcmp(max.buffer, e) > 0) continue;
 
     sim_sb_copy(&max, e);
   }
@@ -41,12 +39,10 @@ static void find_android_llvm(sim_sb *res) {
   sim_sb_path_append(res, "toolchains");
   sim_sb_path_append(res, "llvm");
   sim_sb_path_append(res, "prebuilt");
-  if (!exists(res))
-    sys::die("prebuilt path isn't a directory: [%s]", res->buffer);
+  if (!exists(res)) sys::die("prebuilt path isn't a directory: [%s]", res->buffer);
 
   for (auto e : pprent::list(res->buffer)) {
-    if (e[0] == '.')
-      continue;
+    if (e[0] == '.') continue;
 
     sim_sb_path_append(res, e);
     return;
@@ -68,8 +64,7 @@ static const char *apple_sysroot(const char *sdk) {
   auto path = fgets(buf, sizeof(buf), f);
   pclose(f);
 
-  if (path == nullptr)
-    return nullptr;
+  if (path == nullptr) return nullptr;
 
   path[strlen(path) - 1] = 0; // chomp "\n"
   return path;
@@ -83,28 +78,14 @@ static const char *android_sysroot() {
   return nullptr;
 }
 
-static const char *wasm_sysroot() {
-  const auto sdk = sys::env("WASI_SYSROOT");
-
-  return sdk;
-}
+static const char *wasm_sysroot() { return sys::env("WASI_SYSROOT"); }
 
 static const char *sysroot_for_target(const char *target) {
-  if (IS_TGT(target, TGT_OSX)) {
-    return apple_sysroot("macosx");
-  } else if (IS_TGT(target, TGT_IPHONEOS)) {
-    return apple_sysroot("iphoneos");
-  } else if (IS_TGT(target, TGT_IOS_SIMULATOR)) {
-    return apple_sysroot("iphonesimulator");
-  }
-
-  if (IS_TGT_DROID(target)) {
-    return android_sysroot();
-  }
-  if (IS_TGT(target, TGT_WASM)) {
-    return wasm_sysroot();
-  }
-
+  if (IS_TGT(target, TGT_OSX)) return apple_sysroot("macosx");
+  if (IS_TGT(target, TGT_IPHONEOS)) return apple_sysroot("iphoneos");
+  if (IS_TGT(target, TGT_IOS_SIMULATOR)) return apple_sysroot("iphonesimulator");
+  if (IS_TGT_DROID(target)) return android_sysroot();
+  if (IS_TGT(target, TGT_WASM)) return wasm_sysroot();
   return nullptr;
 }
 
@@ -113,19 +94,12 @@ int main(int argc, char **argv) try {
   bool quiet{};
   auto opts = gopt_parse(argc, argv, "qt:", [&](auto ch, auto val) {
     switch (ch) {
-    case 't':
-      target = val;
-      break;
-    case 'q':
-      quiet = true;
-      break;
-    default:
-      usage();
-      break;
+      case 't': target = val; break;
+      case 'q': quiet = true; break;
+      default: usage(); break;
     }
   });
-  if (opts.argc > 0)
-    usage();
+  if (opts.argc > 0) usage();
 
   sim_sbt cf{};
   sim_sb_path_copy_real(&cf, argv[0]);
@@ -138,8 +112,7 @@ int main(int argc, char **argv) try {
     auto f = sys::fopen(cf.buffer, "r");
     sim_sbt buf{};
     if (fgets(buf.buffer, buf.size, f) != nullptr) {
-      if (!quiet)
-        fwrite(buf.buffer, 1, buf.size, stdout);
+      if (!quiet) fwrite(buf.buffer, 1, buf.size, stdout);
       return 0;
     }
     fclose(f);
@@ -147,8 +120,7 @@ int main(int argc, char **argv) try {
 
   auto sysroot = sysroot_for_target(target);
   if (sysroot) {
-    if (!quiet)
-      puts(sysroot);
+    if (!quiet) puts(sysroot);
 
     auto f = sys::fopen(cf.buffer, "w");
     fputs(sysroot, f);
