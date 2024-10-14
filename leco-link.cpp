375290f662f41cc690024b461d5f7527d3b9d89f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-14 10:08:44 +0100

    link: using sim module

--- leco-link.cpp
+++ leco-link.cpp
@@ -1,5 +1,4 @@
 #pragma leco tool
-#include "sim.hpp"
 #include "targets.hpp"
 
 #include <stdio.h>
@@ -7,6 +6,7 @@
 
 import gopt;
 import mtime;
+import sim;
 import strset;
 import sys;
 
@@ -42,13 +42,10 @@ static void put(const char *a) {
 static constexpr auto max(auto a, auto b) { return a > b ? a : b; }
 
 static void add_local_fw(const char * fw) {
-  sim_sbt stem {};
-  sim_sb_path_copy_stem(&stem, fw);
-  
-  sim_sbt path {};
-  sim_sb_path_copy_parent(&path, fw);
+  auto stem = sim::path_stem(fw);
+  auto path = sim::path_parent(fw);
 
-  fprintf(out, "-F%s\n-framework\n%s\n", path.buffer, stem.buffer);
+  fprintf(out, "-F%s\n-framework\n%s\n", *path, stem.buffer);
 }
 
 static str::map cache{};
@@ -82,12 +79,11 @@ static auto read_dag(const char *dag) {
   });
 
   // Add object after dependencies as this kinda enforces the static init order
-  sim_sbt obj{};
-  sim_sb_copy(&obj, dag);
-  sim_sb_path_set_extension(&obj, "o");
-  put(obj.buffer);
+  sim::sb obj { dag };
+  obj.path_extension("o");
+  put(*obj);
 
-  mtime = max(mtime, mtime::of(obj.buffer));
+  mtime = max(mtime, mtime::of(*obj));
   return mtime;
 }
 
@@ -119,17 +115,15 @@ int main(int argc, char **argv) try {
   if (!input || !output)
     usage();
 
-  sim_sbt path{};
-  sim_sb_path_copy_parent(&path, input);
-  target = sim_sb_path_filename(&path);
+  auto path = sim::path_parent(input);
+  target = path.path_filename();
 
-  sim_sbt args{};
-  sim_sb_copy(&args, input);
-  sim_sb_path_set_extension(&args, "link");
+  sim::sb args { input };
+  args.path_extension("link");
 
   // TODO: move argument build (and mtime check) somewhere else
   //       just in case we want to force a link for any reason
-  out = sys::fopen(args.buffer, "wb");
+  out = sys::fopen(*args, "wb");
   for (auto i = 0; i < opts.argc; i++) {
     fprintf(out, "%s\n", opts.argv[i]);
   }
@@ -151,64 +145,57 @@ int main(int argc, char **argv) try {
   sim_sb_copy(&prev, output);
   sim_sb_concat(&prev, ".old");
 
-  remove(next.buffer);
-  remove(prev.buffer);
+  remove(*next);
+  remove(*prev);
 #endif
 
-  sim_sbt cmd{10240};
-  sim_sb_path_copy_parent(&cmd, argv[0]);
-  sim_sb_path_append(&cmd, "leco-clang.exe");
-  sim_sb_printf(&cmd, " -t %s", target);
-  if (debug)
-    sim_sb_concat(&cmd, " -g");
-  if (opt)
-    sim_sb_concat(&cmd, " -O");
-  sim_sb_printf(&cmd, " -- @%s -o ", args.buffer);
+  sim::sb cmd{10240};
+  cmd.printf(" -t %s", target);
+  if (debug) cmd += " -g";
+  if (opt) cmd += " -O";
+  cmd.printf(" -- @%s -o ", *args);
 #ifdef _WIN32
-  sim_sb_concat(&cmd, next.buffer);
+  cmd += *next;
   // otherwise, face LNK1107 errors from MSVC
-  sim_sb_concat(&cmd, " -fuse-ld=lld");
+  cmd += " -fuse-ld=lld";
 #else
-  sim_sb_concat(&cmd, output);
+  cmd += output;
 #endif
 
   if (IS_TGT(target, TGT_OSX)) {
-    sim_sb_concat(&cmd, " -rpath @executable_path/../Frameworks");
+    cmd += " -rpath @executable_path/../Frameworks";
   } else if (IS_TGT_IOS(target)) {
-    sim_sb_concat(&cmd, " -rpath @executable_path/Frameworks");
+    cmd += " -rpath @executable_path/Frameworks";
   } else if (IS_TGT(target, TGT_WINDOWS)) {
-    sim_sbt rc{};
-    sim_sb_copy(&rc, input);
-    sim_sb_path_set_extension(&rc, "res");
-    if (mtime::of(rc.buffer) > 0) {
-      sim_sb_printf(&cmd, " %s", rc.buffer);
-    }
+    sim::sb rc { input };
+    rc.path_extension("res");
+    if (mtime::of(*rc) > 0) cmd.printf(" %s", *rc);
   } else if (IS_TGT(target, TGT_WASM)) {
-    sim_sbt sra{};
+    char sra[1024] {};
 
     auto f = sys::fopen("../leco/out/wasm32-wasi/sysroot", "r");
-    fgets(sra.buffer, sra.size, f);
+    fgets(sra, sizeof(sra) - 1, f);
     fclose(f);
 
+    cmd.printf(" -resource-dir %s", sra);
+
     // export-table: allows passing lambdas to JS
-    sim_sb_concat(&cmd, " -Xlinker --export-table");
+    cmd += " -Xlinker --export-table";
     // exec-model: without it, we could use "main" but _start calls the global
     //             dtor after "main" returns
-    sim_sb_concat(&cmd, " -mexec-model=reactor");
+    cmd += " -mexec-model=reactor";
     // no-check-features: allows using shared-memory without atomics/etc
     //                    https://stackoverflow.com/a/70186219
     //                    Only works because we won't be using malloc in threads
-    // sim_sb_concat(&cmd, " -Xlinker --shared-memory -Xlinker --no-check-features");
-
-    sim_sb_printf(&cmd, " -resource-dir %s", sra.buffer);
+    // cmd += " -Xlinker --shared-memory -Xlinker --no-check-features";
   }
 
   sys::log("linking", output);
-  sys::run(cmd.buffer);
+  sys::tool_run("clang", "%s", *cmd);
 
 #ifdef _WIN32
-  rename(output, prev.buffer);
-  rename(next.buffer, output);
+  rename(output, *prev);
+  rename(*next, output);
 #endif
 
   return 0;
