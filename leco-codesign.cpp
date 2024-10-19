c90e85efeff69a8ff6a0c9b3d54da8d71dbbb432
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-19 12:32:07 +0100

    codesign: only signing when required

--- leco-codesign.cpp
+++ leco-codesign.cpp
@@ -1,10 +1,43 @@
 #pragma leco tool
+#include <string.h>
 
 import gopt;
+import mtime;
+import pprent;
 import sim;
 import sys;
 
-void usage() { sys::die("invalid usage"); }
+static void usage() { sys::die("invalid usage"); }
+
+struct pair {
+  mtime::t files {};
+  mtime::t signature {};
+};
+
+static pair mod_times(const char * path) {
+  pair res {};
+
+  for (auto entry : pprent::list(path)) {
+    if (entry[0] == '.') continue;
+
+    auto p = sim::sb { path } / entry;
+    auto [ f, s ] = mod_times(*p);
+    res.files = res.files < f ? f : res.files;
+    res.signature = res.signature < s ? s : res.signature;
+  }
+
+  auto f = mtime::of(path);
+  res.files = res.files < f ? f : res.files;
+
+  if (strstr(path, "_CodeSignature")) {
+    res.signature = res.signature < f ? f : res.signature;
+  }
+  return res;
+}
+static bool sign_is_fresh(const char * path) {
+  auto [ f, s ] = mod_times(path);
+  return f == s;
+}
 
 int main(int argc, char ** argv) try {
   const char * path {};
@@ -15,6 +48,8 @@ int main(int argc, char ** argv) try {
     }
   });
   if (!path || opts.argc) usage();
+
+  if (sign_is_fresh(path)) return 0;
 
   sys::log("codesign", path);
   sys::runf("codesign -f -s %s %s", sys::env("LECO_IOS_TEAM"), path);
