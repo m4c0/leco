a4cee66f3f3691cb6f0f46783c14c172fb30d3db
Eduardo <m4c0@users.noreply.github.com>
2024-11-02 12:20:07 +0000

    git-std using sim module

--- leco-git-std.cpp
+++ leco-git-std.cpp
@@ -1,10 +1,10 @@
 #pragma leco tool
-#include "sim.hpp"
 
 #include <stdio.h>
 
 import gopt;
 import mtime;
+import sim;
 import sys;
 
 static void usage() {
@@ -23,30 +23,17 @@ Where:
 )");
 }
 
-static void clone(const sim_sb *name) {
-  auto fn = sim_sb_path_filename(name);
-
-  sim_sbt cmd{10240};
-  sim_sb_printf(&cmd, "git clone git@github.com:m4c0/%s.git %s", fn,
-                name->buffer);
-  sys::run(cmd.buffer);
+static void clone(const sim::sb & name) {
+  sys::runf("git clone git@github.com:m4c0/%s.git %s", name.path_filename(), *name);
 }
 
-static void set_email(const sim_sb *name) {
-  sim_sbt cmd{10240};
-  sim_sb_printf(&cmd, "git -C %s config user.name 'Eduardo Costa'",
-                name->buffer);
-  sys::run(cmd.buffer);
-
-  sim_sb_copy(&cmd, "");
-  sim_sb_printf(&cmd,
-                "git -C %s config user.email 'm4c0@users.noreply.github.com'",
-                name->buffer);
-  sys::run(cmd.buffer);
+static void set_email(const sim::sb & name) {
+  sys::runf("git -C %s config user.name 'Eduardo Costa'", *name);
+  sys::runf("git -C %s config user.email 'm4c0@users.noreply.github.com'", *name);
 }
 
-static void setup_precommit(const sim_sb *name) {
-  auto f = sys::fopen(name->buffer, "w");
+static void setup_precommit(const sim::sb & name) {
+  auto f = sys::fopen(*name, "w");
   fprintf(f, R"(#!/bin/sh
 
 ../leco/leco.exe format -n
@@ -55,41 +42,27 @@ static void setup_precommit(const sim_sb *name) {
 }
 
 int main(int argc, char **argv) try {
-  sim_sbt name{};
+  sim::sb name = "."_real;
   auto opts = gopt_parse(argc, argv, "i:", [&](auto ch, auto val) {
     switch (ch) {
-    case 'i':
-      sim_sb_path_copy_real(&name, val);
-      break;
-    default:
-      usage();
+      case 'i': name = sim::path_real(val); break;
+      default: usage();
     }
   });
+  if (opts.argc) usage();
 
-  if (opts.argc)
-    usage();
+  if (mtime::of(*name) == 0) clone(name);
+  set_email(name);
 
-  if (name.len == 0)
-    sim_sb_path_copy_real(&name, ".");
-
-  if (mtime::of(name.buffer) == 0)
-    clone(&name);
-
-  set_email(&name);
-
-  sim_sb_path_append(&name, ".git");
-  sim_sb_path_append(&name, "hooks");
-  sim_sb_path_append(&name, "pre-commit");
-  if (mtime::of(name.buffer) > 0) {
+  name = name / ".git" / "hooks" / "pre-commit";
+  if (mtime::of(*name) > 0) {
     fprintf(stderr, "pre-commit hook exists... bailing out...\n");
   } else {
-    setup_precommit(&name);
+    setup_precommit(name);
   }
 
 #ifndef _WIN32
-  sim_sbt chmod{10240};
-  sim_sb_printf(&chmod, "chmod a+x %s", name.buffer);
-  sys::run(chmod.buffer);
+  sys::runf("chmod a+x %s", *name);
 #endif
 } catch (...) {
   return 1;
