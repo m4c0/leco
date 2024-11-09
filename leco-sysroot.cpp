e1fefec7163333aa748e9a894448dc131d3108bf
Eduardo <m4c0@users.noreply.github.com>
2024-11-09 12:48:11 +0000

    sysroot: using sim module

--- leco-sysroot.cpp
+++ leco-sysroot.cpp
@@ -1,6 +1,5 @@
 #pragma leco tool
 
-#include "sim.hpp"
 #include "targets.hpp"
 
 #include <stdio.h>
@@ -9,84 +8,70 @@
 import gopt;
 import mtime;
 import pprent;
+import sim;
 import sys;
 
 void usage() { sys::die("invalid usage"); }
 
-static bool exists(const sim_sb *path) { return mtime::of(path->buffer) > 0; }
+static bool exists(const char * path) { return mtime::of(path) > 0; }
 
-static void find_android_llvm(sim_sb *res) {
-  const auto sdk = sys::env("ANDROID_SDK_ROOT");
+static sim::sb android_sysroot() {
+  const sim::sb sdk { sys::env("ANDROID_SDK_ROOT") };
 
-  sim_sb_path_copy_append(res, sdk, "ndk-bundle");
-  if (exists(res)) return;
+  auto res = sdk / "ndk-bundle";
+  if (exists(*res)) return res;
 
-  sim_sb_path_copy_append(res, sdk, "ndk");
-  if (!exists(res))
-    sys::die("ANDROID_SDK_ROOT path does not contain a folder named 'sdk': [%s]",
-        res->buffer);
+  res = sdk / "ndk";
+  if (!exists(*res))
+    sys::die("ANDROID_SDK_ROOT path does not contain a folder named: [%s]", *res);
 
-  sim_sbt max{};
-  sim_sb_copy(&max, "");
+  sim::sb max { "" };
 
-  for (auto e : pprent::list(res->buffer)) {
-    if (strcmp(max.buffer, e) > 0) continue;
-
-    sim_sb_copy(&max, e);
+  for (auto e : pprent::list(*res)) {
+    if (strcmp(*max, e) > 0) continue;
+    max = sim::sb { e };
   }
 
-  sim_sb_path_append(res, max.buffer);
-  sim_sb_path_append(res, "toolchains");
-  sim_sb_path_append(res, "llvm");
-  sim_sb_path_append(res, "prebuilt");
-  if (!exists(res)) sys::die("prebuilt path isn't a directory: [%s]", res->buffer);
+  res = res / *max / "toolchains" / "llvm" / "prebuilt";
+  if (!exists(*res)) sys::die("prebuilt path isn't a directory: [%s]", *res);
 
-  for (auto e : pprent::list(res->buffer)) {
+  for (auto e : pprent::list(*res)) {
     if (e[0] == '.') continue;
 
-    sim_sb_path_append(res, e);
-    return;
+    return res / e / "sysroot";
   }
-  sys::die("no LLVM inside prebuilt dir: [%s]", res->buffer);
+  sys::die("no LLVM inside prebuilt dir: [%s]", *res);
 }
 
-static const char *apple_sysroot(const char *sdk) {
+static sim::sb apple_sysroot(const char *sdk) {
 #ifndef __APPLE__
   sys::die("apple targets not supported when host isn't osx");
-  return nullptr;
+  return {};
 #else
-  sim_sbt cmd{};
-  sim_sb_printf(&cmd, "xcrun --show-sdk-path --sdk %s", sdk);
+  auto cmd = sim::printf("xcrun --show-sdk-path --sdk %s", sdk);
 
-  static char buf[256];
+  sim::sbt buf {};
 
-  auto f = popen(cmd.buffer, "r");
-  auto path = fgets(buf, sizeof(buf), f);
+  auto f = popen(*cmd, "r");
+  auto path = fgets(*buf, buf.len, f);
   pclose(f);
 
-  if (path == nullptr) return nullptr;
+  if (path == nullptr) return {};
 
-  path[strlen(path) - 1] = 0; // chomp "\n"
-  return path;
+  (*buf)[--buf.len] = 0; // chomp "\n"
+  return buf;
 #endif
 }
 
-static const char *android_sysroot() {
-  sim_sbt llvm{};
-  find_android_llvm(&llvm);
-  sim_sb_path_append(&llvm, "sysroot");
-  return nullptr;
-}
+static sim::sb wasm_sysroot() { return sim::sb { sys::env("WASI_SYSROOT") }; }
 
-static const char *wasm_sysroot() { return sys::env("WASI_SYSROOT"); }
-
-static const char *sysroot_for_target(const char *target) {
+static sim::sb sysroot_for_target(const char *target) {
   if (IS_TGT(target, TGT_OSX)) return apple_sysroot("macosx");
   if (IS_TGT(target, TGT_IPHONEOS)) return apple_sysroot("iphoneos");
   if (IS_TGT(target, TGT_IOS_SIMULATOR)) return apple_sysroot("iphonesimulator");
   if (IS_TGT_DROID(target)) return android_sysroot();
   if (IS_TGT(target, TGT_WASM)) return wasm_sysroot();
-  return nullptr;
+  return {};
 }
 
 int main(int argc, char **argv) try {
@@ -101,29 +86,28 @@ int main(int argc, char **argv) try {
   });
   if (opts.argc > 0) usage();
 
-  sim_sbt cf{};
-  sim_sb_path_copy_real(&cf, argv[0]);
-  sim_sb_path_parent(&cf);
-  sim_sb_path_parent(&cf);
-  sim_sb_path_append(&cf, target);
-  sys::mkdirs(cf.buffer);
-  sim_sb_path_append(&cf, "sysroot");
-  if (mtime::of(cf.buffer) > 0) {
-    auto f = sys::fopen(cf.buffer, "r");
-    sim_sbt buf{};
-    if (fgets(buf.buffer, buf.size, f) != nullptr) {
-      if (!quiet) fwrite(buf.buffer, 1, buf.size, stdout);
+  auto cf = sim::path_real(argv[0]);
+  cf.path_parent();
+  cf.path_parent();
+  cf /= target;
+  sys::mkdirs(*cf);
+  cf /= "sysroot";
+  if (exists(*cf)) {
+    auto f = sys::fopen(*cf, "r");
+    char buf[10240] {};
+    if (fgets(buf, sizeof(buf), f) != nullptr) {
+      if (!quiet) fwrite(buf, 1, sizeof(buf), stdout);
       return 0;
     }
     fclose(f);
   }
 
   auto sysroot = sysroot_for_target(target);
-  if (sysroot) {
-    if (!quiet) puts(sysroot);
+  if (sysroot.len) {
+    if (!quiet) puts(*sysroot);
 
-    auto f = sys::fopen(cf.buffer, "w");
-    fputs(sysroot, f);
+    auto f = sys::fopen(*cf, "w");
+    fputs(*sysroot, f);
     fclose(f);
   }
 } catch (...) {
