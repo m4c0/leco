36b72115e23bd109f942ddbf16e801d00460c063
Eduardo <m4c0@users.noreply.github.com>
2024-12-24 11:52:17 +0000

    meta: modernise a bit

--- leco-meta.cpp
+++ leco-meta.cpp
@@ -1,60 +1,49 @@
 #pragma leco tool
-#include "sim.h"
 #include "targets.hpp"
 
 #include <stdlib.h>
 
 import gopt;
 import mtime;
+import sim;
 import sys;
 import sysstd;
+
+static void chdir(const char * dir) {
+  if (0 == sysstd::chdir(dir)) return;
+  sys::die("Directory not found: [%s]\n", dir);
+}
 
 int main(int argc, char ** argv) try {
 #ifdef _WIN32
   system(""); // enable ANSI colours
 #endif
 
-  sim_sb cmd {};
-  sim_sb_new(&cmd, 10240);
-  sim_sb_path_copy_real(&cmd, argv[0]);
-  sim_sb_path_parent(&cmd);
-
   auto opts = gopt_parse(argc, argv, "C:", [&](auto ch, auto val) {
     switch (ch) {
-      case 'C':
-        if (0 != sysstd::chdir(val)) {
-          sys::die("Directory not found: [%s]\n", val);
-        }
-        break;
+      case 'C': chdir(val); break;
       default: break;
     }
   });
 
   if (opts.argc == 0) {
-    sim_sb_path_append(&cmd, "leco-driver.exe");
-    sys::run(cmd.buffer);
+    sys::tool_run("driver");
     return 0;
   }
 
-  sim_sb_path_append(&cmd, "leco-");
-  sim_sb_concat(&cmd, opts.argv[0]);
-  sim_sb_concat(&cmd, ".exe");
-
-  if (mtime::of(cmd.buffer) > 0) {
+  auto cmd = sys::tool_cmd(opts.argv[0]);
+  if (mtime::of(*cmd) > 0) {
     opts.argc--;
     opts.argv++;
   } else {
-    sim_sb_path_parent(&cmd);
-    sim_sb_path_append(&cmd, "leco-driver.exe");
+    cmd = sys::tool_cmd("driver");
   }
 
   // TODO: escape arguments
-  for (auto i = 0; i < opts.argc; i++) {
-    sim_sb_concat(&cmd, " ");
-    sim_sb_concat(&cmd, opts.argv[i]);
-  }
+  for (auto i = 0; i < opts.argc; i++)
+    cmd.printf(" %s", opts.argv[i]);
 
-  sys::run(cmd.buffer);
+  sys::run(*cmd);
 } catch (...) {
   return 1;
 }
