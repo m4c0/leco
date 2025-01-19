da2e6a4ec7552eabea4a4c8eac7c5423776a8701
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-19 12:17:59 +0000

    args: implementation based on deplist

--- leco-args.cpp
+++ leco-args.cpp
@@ -1,6 +1,8 @@
 #pragma leco tool
+#include <stdio.h>
 
 import gopt;
+import mtime;
 import sim;
 import strset;
 import sys;
@@ -16,17 +18,33 @@ Where:
 )");
 }
 
+static void print(FILE * out, const char * file) {
+  if (!out) return;
+  auto stem = sim::path_stem(file);
+  sim::sb pcmf { file };
+  pcmf.path_extension("pcm");
+  fprintf(out, "-fmodule-file=%s=%s\n", *stem, *pcmf);
+}
+
 static str::set recursed {};
 static void recurse(const char * dag) {
   if (!recursed.insert(dag)) return;
 
-  sys::dag_read(dag, [](auto id, auto file) {
+  auto af = sim::sb { dag };
+  af.path_extension("args");
+  FILE * out {};
+  if (mtime::of(dag) > mtime::of(*af)) out = sys::fopen(*af, "wb");
+
+  sys::dag_read(dag, [out](auto id, auto file) {
     switch (id) {
       case 'idag':
-      case 'mdag': recurse(file); break;
+      case 'mdag': print(out, file); recurse(file); break;
+      case 'idir': if (out) fprintf(out, "-I%s\n", file); break;
       default: break;
     }
   });
+
+  sys::fclose(out);
 }
 
 int main(int argc, char ** argv) try {
