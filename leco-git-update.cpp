8c4939628d85c54d325de54c18ddb93fda74233d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-31 16:00:49 +0000

    git-update using rec_dag

--- leco-git-update.cpp
+++ leco-git-update.cpp
@@ -13,31 +13,6 @@ import sys;
 static const char *target{HOST_TARGET};
 static const char *argv0;
 
-static str::set unique_parents{};
-
-static str::set added{};
-static void read_dag(const char *dag) {
-  if (!added.insert(dag))
-    return;
-
-  sys::dag_read(dag, [](auto id, auto file) {
-    switch (id) {
-    case 'idag':
-    case 'mdag': {
-      read_dag(file);
-      break;
-    }
-    default:
-      break;
-    }
-  });
-
-  auto parent = sim::path_parent(dag);
-  parent.path_parent();
-  parent.path_parent();
-  unique_parents.insert(*parent);
-}
-
 static void usage() {
   sys::die(R"(
 Runs a "git pull" for each of the collected dependencies of the given target.
@@ -55,24 +30,21 @@ int main(int argc, char **argv) try {
   argv0 = argv[0];
   auto opts = gopt_parse(argc, argv, "t:", [&](auto ch, auto val) {
     switch (ch) {
-    case 't':
-      target = val;
-      break;
-    default:
-      usage();
+      case 't': target = val; break;
+      default: usage();
     }
   });
-  if (opts.argc != 0)
-    usage();
+  if (opts.argc != 0) usage();
 
-  auto cwd = "out"_real / target;
-  for (auto entry : pprent::list(cwd.buffer)) {
-    auto ext = sim::path_extension(entry);
-    if (!ext.len || (0 != strcmp(".dag", *ext)))
-      continue;
+  str::set unique_parents {};
+  sys::for_each_dag(target, true, [&](auto dag, auto id, auto file) {
+    if (id != 'vers') return;
 
-    read_dag(*(cwd / entry));
-  }
+    auto parent = sim::path_parent(dag);
+    parent.path_parent();
+    parent.path_parent();
+    unique_parents.insert(*parent);
+  });
 
   for (auto & parent : unique_parents) {
     sys::runf("git -C %s pull", parent.c_str());
