bfaaef7dbcffa03e30be3eb7cff38a1001f6f06c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-13 19:03:51 +0000

    help: updating some tools

--- leco-help.cpp
+++ leco-help.cpp
@@ -5,29 +5,32 @@ int main(int argc, char **argv) {
   fprintf(stderr, R"(
 Usage: ../leco/leco.exe <tool> <tool-options>
 
-Existing tools:
+Porcelain tools:
 
         cleaner: cleans output folders
         driver: drives the LECO workflow (default tool if unspecified)
         format: formats code using clang-format
-        git-std: applies some "standard options" to a git repo
         git-status: list status of all folders in the git workspace
         git-update: recursively updates known dependencies
         help: this tool
         ipa-upload: validates/upload iOS binaries
 
-Low-level plumbing tools:
+Plumbing tools:
 
         bundler: bundles applications
         clang: runs clang with opiniated defaults
+        codesign: runs codesign on Apple platforms
         dagger: generates dependency files based on parsed code
         deplist: list dependent c++ modules
         exs: copies dependent executables to app bundles
         ipa: generates and signs IPAs
         link: links an executable
         recurse: compiles C++ code recursively
+        rsrc: copies resource files to their app's resource folder
         sawblade: runs dagger recursively
+        shaders: compiles GLSL into SPIRV
         sysroot: calculates the sysroot of the environment
+        wasm-js: almost a webpacker
         xcassets: generates xcassets for IPAs
 
 )");
