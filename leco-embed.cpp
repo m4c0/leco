2f6ce5052bcf18b2ab274c40fbb3d39d39fdf0f9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-18 19:27:59 +0000

    embed: basic boilerplate for traversing files

--- leco-embed.cpp
+++ leco-embed.cpp
@@ -1,4 +1,23 @@
 #pragma leco tool
 
+import gopt;
+import sys;
+
+static void usage() { sys::die("invalid usage"); }
+
 int main(int argc, char ** argv) {
+  const char * target = nullptr;
+  auto opts = gopt_parse(argc, argv, "t:", [&](auto ch, auto var) {
+    switch (ch) {
+      case 't': target = var; break;
+      default: usage();
+    }
+  });
+  if (!target || opts.argc) usage();
+
+  sys::for_each_dag(target, true, [](const char * dag, auto id, auto file) {
+    if (id != 'embd') return;
+    sys::log("file", file);
+    sys::log("dag", dag);
+  });
 }
