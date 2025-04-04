dc29b9ca3f824367c0bd058b5037df6e06ed33c4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-04 12:04:20 +0100

    meta: cleanup/reformat

--- leco-meta.cpp
+++ leco-meta.cpp
@@ -10,8 +10,7 @@ import sys;
 import sysstd;
 
 static void chdir(const char * dir) {
-  if (0 == sysstd::chdir(dir)) return;
-  sys::die("Directory not found: [%s]\n", dir);
+  if (0 != sysstd::chdir(dir)) sys::die("Directory not found: [%s]\n", dir);
 }
 
 int main(int argc, char ** argv) try {
@@ -20,16 +19,10 @@ int main(int argc, char ** argv) try {
 #endif
 
   auto opts = gopt_parse(argc, argv, "C:", [&](auto ch, auto val) {
-    switch (ch) {
-      case 'C': chdir(val); break;
-      default: break;
-    }
+    if (ch == 'C') chdir(val);
   });
 
-  if (opts.argc == 0) {
-    sys::tool_run("driver");
-    return 0;
-  }
+  if (opts.argc == 0) return sys::tool_run("driver");
 
   auto cmd = sys::tool_cmd(opts.argv[0]);
   if (mtime::of(*cmd) > 0) {
@@ -39,7 +32,7 @@ int main(int argc, char ** argv) try {
     cmd = sys::tool_cmd("driver");
   }
 
-  // TODO: escape arguments
+  // TODO: escape arguments or replace with execvp
   for (auto i = 0; i < opts.argc; i++)
     cmd.printf(" %s", opts.argv[i]);
 
