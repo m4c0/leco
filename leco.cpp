e09a7f2a74a8e070be5faeac3c5a3b1490b60b7a
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-07 11:50:05 +0100

    phase 1: execv trickery

--- leco.cpp
+++ leco.cpp
@@ -6,13 +6,11 @@
 #define SEP SIM_PATHSEP_S
 #define CMD ".." SEP "leco" SEP "out" SEP HOST_TARGET SEP "leco-meta.exe"
 
+// Using Windows definition for extra constness
+extern "C" int execv(const char *, const char * const *);
+
 int main(int argc, char ** argv) {
-  sim_sb cmd {};
-  sim_sb_new(&cmd, 10240);
-  sim_sb_copy(&cmd, CMD);
-  for (auto i = 1; i < argc; i++) {
-    // TODO: escape
-    sim_sb_printf(&cmd, " %s", argv[i]);
-  }
-  return system(cmd.buffer);
+  if (argv[argc]) return 69;
+  argv[0] = strdup(CMD);
+  return execv(argv[0], argv);
 }
