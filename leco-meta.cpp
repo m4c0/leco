a6a868b6b7e4dd7837ff8f7a796c2c813637ddc0
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-07 12:32:32 +0100

    meta: using shift+execv

--- leco-meta.cpp
+++ leco-meta.cpp
@@ -4,23 +4,33 @@ import sim;
 import sys;
 import sysstd;
 
+// Using Windows definition for extra constness
+extern "C" int execv(const char *, char * const *);
+
 static void chdir(const char * dir) {
   if (0 != sysstd::chdir(dir)) sys::die("Directory not found: [%s]\n", dir);
 }
 
+extern "C" int puts(const char *);
 int main(int argc, char ** argv) try {
+  if (argv[argc]) return 69;
+
 #ifdef _WIN32
   system(""); // enable ANSI colours
 #endif
 
-  const auto shift = [&] { return argc ? "" : (argc--, *++argv); };
-  argc--;
+  const auto shift = [&] { return argc ? (argc--, *++argv) : nullptr; };
+  while (auto val = shift())
+    if (sim::sb{"-C"} == val) chdir(shift());
+    else break;
 
-  if (sim::sb{"-C"} == *argv) chdir((shift(), shift()));
-
-  auto cmd = sys::tool_cmd(argc && mtime::of(*argv) ? shift() : "driver");
-  while (argc) cmd.printf(" %s", shift());
-  sys::run(*cmd);
+  auto cmd = sys::tool_cmd(*argv ? *argv : "");
+  if (!mtime::of(*cmd)) {
+    argv--;
+    cmd = sys::tool_cmd("driver");
+  }
+  *argv = *cmd;
+  execv(*argv, argv);
 } catch (...) {
   return 1;
 }
