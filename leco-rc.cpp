92085042fc4e40c00a14f6ec0416464d5532093e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-22 10:44:24 +0100

    rc: minor refactorings

--- leco-rc.cpp
+++ leco-rc.cpp
@@ -7,20 +7,17 @@ import sys;
 
 static void usage() { sys::die("invalid usage"); }
 
-void run(const char * input) {
+void run(const char * dag) {
   sim::sb rc {};
-  sys::dag_read(input, [&](auto id, auto file) {
-    switch (id) {
-      case 'srcf': rc = sim::sb { file }; break;
-      default: break;
-    }
+  sys::dag_read(dag, [&](auto id, auto file) {
+    if (id == 'srcf') rc = sim::sb { file };
   });
 
-  if (rc == "") sys::die("invalid dag file");
+  if (rc == "") sys::die("dag file without source information");
   rc.path_extension("rc");
   if (mtime::of(*rc) == 0) return;
 
-  sim::sb res { input };
+  sim::sb res { dag };
   res.path_extension("res");
   sys::runf("llvm-rc.exe /FO %s %s", *res, *rc);
 }
