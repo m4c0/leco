94910934fe521a20adccf8355d8872cf96d16fb4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-06 19:14:37 +0100

    deplist: nit

--- leco-deplist.cpp
+++ leco-deplist.cpp
@@ -19,17 +19,6 @@ static void print_pcm(const char * pcmf) {
   fprintf(out, "-fmodule-file=%s=%s\n", *stem, *pcm);
 }
 
-static void read_dag(const char *dag) {
-  sys::recurse_dag(dag, [](auto dag, auto id, auto file) {
-    if (id == 'pcmf') print_pcm(file);
-  });
-}
-static void read_includes(const char * dag) {
-  sys::dag_read(dag, [](auto id, auto file) {
-    if (id == 'idir') fprintf(out, "-I%s\n", file);
-  });
-}
-
 int main() try {
   sys::for_each_dag(true, [](auto dag, auto id, auto file) {
     if (id != 'vers') return;
@@ -45,14 +34,18 @@ int main() try {
     output.path_extension("deps");
     if (mtime::of(dag) > mtime::of(*output)) {
       out = sys::fopen(*output, "wb");
-      read_dag(dag);
+      sys::recurse_dag(dag, [](auto dag, auto id, auto file) {
+        if (id == 'pcmf') print_pcm(file);
+      });
       fclose(out);
     }
 
     output.path_extension("incs");
     if (mtime::of(dag) > mtime::of(*output)) {
       out = sys::fopen(*output, "wb");
-      read_includes(dag);
+      sys::dag_read(dag, [](auto id, auto file) {
+        if (id == 'idir') fprintf(out, "-I%s\n", file);
+      });
       fclose(out);
     }
   });
