e6f8f467e44f27438138f6c9a78edeba0ea32ae4
Eduardo <m4c0@users.noreply.github.com>
2025-05-18 12:14:08 +0100

    rc: refactoring

--- leco-rc.cpp
+++ leco-rc.cpp
@@ -2,24 +2,20 @@
 
 import sys;
 
-void run(const char * dag, const char * file) {
-  sim::sb rc { file };
-  rc.path_extension("rc");
-  if (mtime::of(*rc) == 0) return;
-
-  sim::sb res { dag };
-  res.path_extension("res");
-  sys::runf("llvm-rc.exe /FO %s %s", *res, *rc);
-}
-
 int main() try {
   if (!sys::is_tgt_windows()) return 0;
 
-  sys::for_each_dag(false, [](auto * dag, auto id, auto file) {
+  sys::for_each_dag(false, [](auto * dag, auto id, auto) {
     if (id != 'tapp') return;
 
-    sys::dag_read(dag, [&](auto id, auto file) {
-      if (id == 'srcf') run(dag, file);
+    sys::dag_read(dag, [dag](auto id, auto file) {
+      if (id != 'srcf') return;
+
+      auto rc = sim::sb { file }.path_extension("rc");
+      if (mtime::of(*rc) == 0) return;
+
+      auto res = sim::sb { dag }.path_extension("res");
+      sys::run("llvm-rc.exe /FO", *res, *rc);
     });
   });
 } catch (...) {
