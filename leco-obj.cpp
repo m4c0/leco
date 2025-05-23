20b98761694b009c7750558907f97ecab75aeece
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-23 13:04:55 +0100

    obj: simpler logic

--- leco-obj.cpp
+++ leco-obj.cpp
@@ -17,44 +17,42 @@ static void compile(const char * src, const char * obj, const char * dag) {
   sys::tool_run("clang", "-i %s -- %s -c -o %s @%s @%s", src, lang, obj, *deps, *incs);
 }
 
-static str::set done {};
-static void process(const char * dag) {
-  if (!done.insert(dag)) return;
+static str::map spec_cache {};
+static auto calc_mtime(const char * dag) {
+  auto &mtime = spec_cache[dag];
+  if (mtime != 0) return mtime;
+  mtime = mtime::of(dag);
 
-  auto mtime = 0ULL;
-  bool is_pcm = false;
+  sys::dag_read(dag, [&](auto id, auto file) {
+    switch (id) {
+      case 'head': mtime = max(mtime, mtime::of(file)); break;
+      case 'mdag': mtime = max(mtime, calc_mtime(file)); break;
+    }
+  });
 
+  return mtime;
+}
+
+static void compile(const char * dag) {
+  bool pcm = false;
   sim::sb src {};
   sim::sb obj {};
   sys::dag_read(dag, [&](auto id, auto file) {
     switch (id) {
-      case 'head': mtime = max(mtime, mtime::of(file)); break;
-
-      case 'idag':
-      case 'mdag': process(file); break;
-
-      case 'pcmf': is_pcm = true; break;
-
+      case 'pcmf': pcm = true; break;
       case 'srcf': src = sim::sb { file }; break;
       case 'objf': obj = sim::sb { file }; break;
-      default: break;
     }
   });
+  if (pcm) return;
+  if (calc_mtime(dag) < mtime::of(*obj)) return;
 
-  if (is_pcm) return;
-  mtime = max(mtime, mtime::of(*src));
-
-  if (src.len == 0) sys::die("dag without source info: [%s]", dag);
-  if (obj.len == 0) sys::die("dag without object info: [%s]", dag);
-
-  if (mtime > mtime::of(*obj)) {
-    compile(*src, *obj, dag);
-  }
+  compile(*src, *obj, dag);
 }
 
 int main() try {
-  sys::for_each_root_dag([](auto * dag, auto id, auto file) {
-    process(dag);
+  sys::for_each_dag(true, [](auto * dag, auto id, auto file) {
+    if (id == 'objf') compile(dag);
   });
 } catch (...) {
   return 1;
