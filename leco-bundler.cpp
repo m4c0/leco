7905f7fa99b99c69280e16b4f251407f500594a2
Eduardo Costa <m4c0@users.noreply.github.com>
2025-06-05 12:06:29 +0100

    bundler: removing wasm logic

--- leco-bundler.cpp
+++ leco-bundler.cpp
@@ -2,23 +2,13 @@
 
 import sys;
 
-static void wasm_bundle(const char *dag) {
-  auto path = sim::sb { dag }.path_extension("app");
-
-  path /= sim::path_filename(dag);
-  path.path_extension("html");
-  sys::link("../leco/wasm.html", *path);
-
-  path.path_parent();
-  sys::tool_run("wasm-js", " -i %s", dag);
-}
-
 static void bundle(const char * dag, const char * _) {
-  if      (sys::is_tgt_apple()) sys::tool_run("ipa", "-i %s", dag);
-  else if (sys::is_tgt_wasm())  wasm_bundle(dag);
+  if (sys::is_tgt_apple()) sys::tool_run("ipa", "-i %s", dag);
 }
 
 int main() try {
+  if (sys::is_tgt_wasm()) sys::tool_run("wasm-js");
+
   sys::for_each_tag_in_dags('tapp', false, &bundle);
   return 0;
 } catch (...) {
