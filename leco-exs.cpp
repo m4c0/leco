b2e793f2f76df57debfc69140b3d94787e1c620d
Eduardo <m4c0@users.noreply.github.com>
2025-07-06 13:02:45 +0100

    exs: polishing the "code intent"

--- leco-exs.cpp
+++ leco-exs.cpp
@@ -10,11 +10,6 @@ static void copy_exe(const sim::sb & exedir, const char * input) {
   sys::link(input, *path);
 }
 
-static void copy_plgn(const sim::sb & exedir, const char * dll_dag) {
-  auto tdll = sys::read_dag_tag('tdll', dll_dag);
-  copy_exe(exedir, *tdll);
-}
-
 static void copy_xcfw(const sim::sb & exedir, const char * xcfw_path) {
   sim::sb tgt = exedir;
   if (!sys::is_tgt_ios()) tgt.path_parent();
@@ -28,6 +23,20 @@ static void copy_xcfw(const sim::sb & exedir, const char * xcfw_path) {
   sys::tool_run("codesign", "-d %s", *tgt);
 }
 
+static void copy_plugins(const sim::sb & exedir, const char * dag) {
+  sys::dag_read(dag, [&](auto id, auto file) {
+    if (id != 'plgn') return;
+    copy_exe(exedir, *sys::read_dag_tag('tdll', file));
+  });
+}
+
+static void copy_libraries(const sim::sb & exedir, const char * dag) {
+  sys::recurse_dag(dag, [&](auto dag, auto id, auto file) {
+    if (id == 'dlls') return copy_exe(exedir, file);
+    if (id == 'xcfw') return copy_xcfw(exedir, file);
+  });
+}
+
 int main(int argc, char ** argv) try {
   sys::for_each_root_dag([](auto dag, auto id, auto file) {
     if (id != 'tapp' && id != 'tool') return;
@@ -38,15 +47,8 @@ int main(int argc, char ** argv) try {
     sys::mkdirs(*edir);
 
     copy_exe(edir, file);
-
-    sys::dag_read(dag, [&](auto id, auto file) {
-      if (id == 'plgn') return copy_plgn(edir, file);
-    });
-
-    sys::recurse_dag(dag, [&](auto dag, auto id, auto file) {
-      if (id == 'dlls') return copy_exe(edir, file);
-      if (id == 'xcfw') return copy_xcfw(edir, file);
-    });
+    copy_plugins(edir, dag);
+    copy_libraries(edir, dag);
   });
 } catch (...) {
   return 1;
