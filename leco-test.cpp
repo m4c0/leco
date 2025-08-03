0882c2e82decbbe9d40268822ff5f6ba5218cd4b
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-03 15:52:53 +0100

    test: returning 1 instead of crashing on exceptions

--- leco-test.cpp
+++ leco-test.cpp
@@ -2,10 +2,12 @@
 
 import sys;
 
-int main() {
+int main() try {
   sys::for_each_root_dag([](auto dag, auto id, auto file) {
     if (id != 'test') return;
     sys::log("running", file);
     sys::run(file);
   });
+} catch (...) {
+  return 1;
 }
