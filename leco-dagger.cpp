380f1aafcc6b47b1ca50ca69f6843d209b416c29
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-05 19:34:31 +0100

    dagger: removing "--" from clang call

--- leco-dagger.cpp
+++ leco-dagger.cpp
@@ -411,9 +411,7 @@ static bool pragma(const char * p) {
 
 enum run_result { OK, ERR, SKIPPED };
 [[nodiscard]] static run_result run(const char * dag, const char * src, bool roots_only) try {
-  p::proc proc {
-    *sys::tool_cmd("clang"), "--", src, "-E"
-  };
+  p::proc proc { *sys::tool_cmd("clang"), src, "-E" };
   sys::file f { dag, "w" };
 
   source = sim::sb { src };
