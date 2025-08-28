46fe889814bf77633e4203de4eda83f7457d9f66
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-28 18:17:08 +0100

    run: hacking a way to run wasm targets

--- leco-run.cpp
+++ leco-run.cpp
@@ -22,8 +22,40 @@ int run_target(const char * name, int argc, char ** argv) {
   return mct_syscall_spawn(*argv, argv);
 }
 
+static void list_wasm_targets() {
+  sys::for_each_root_dag([](auto dag, auto id, auto file) {
+    if (id == 'tapp') putln(*sim::path_stem(dag));
+  });
+}
+
+static void run_wasm_target(const char * name, int argc, char ** argv) {
+  sim::sb edir {};
+  sys::for_each_root_dag([&](auto dag, auto id, auto file) {
+    if (sim::path_stem(dag) != name) return;
+    if (id != 'tapp') die("only app bundles are supported");
+    edir = sys::read_dag_tag('edir', dag);
+  });
+  if (edir == "") die("invalid target: ", name);
+
+  if (0 != mct_syscall_chdir(*edir)) die("missing bundle directory");
+
+  // TODO: detect python3 and use a less-hardcoded path
+  const char * args[] {
+    "python3",
+    "../../../../leco/webserver.py",
+    0,
+  };
+  if (0 != mct_syscall_spawn(*args, args)) die("webserver failed");
+}
+
+static void main_wasm(int argc, char ** argv) {
+  if (argc == 1) return list_wasm_targets();
+  else run_wasm_target(argv[1], argc - 1, argv + 1);
+}
+
 int main(int argc, char ** argv) try {
   if (argv[argc]) return 69;
+  if (sys::is_tgt_wasm()) return (main_wasm(argc, argv), 0);
   if (!sys::is_tgt_host()) die("Only host targets are supporter at the moment");
 
   if (argc == 1) return (list_targets(), 0);
