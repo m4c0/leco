792ab44114d0be130fff6380eef5466d3d596c87
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-31 14:35:01 +0100

    driver: removing unused check

--- leco-driver.cpp
+++ leco-driver.cpp
@@ -93,7 +93,6 @@ int main(int argc, char ** argv) try {
     else if ("-O"_s == val) mct_syscall_setenv("LECO_OPT", "1");
     else usage();
   }
-  if (!target) usage();
 
   run_for(sim::sb{target});
   return 0;
