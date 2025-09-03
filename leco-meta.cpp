a78cb8b4a000a315f49e2c82253ecfe0fa6003b2
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-03 17:14:45 +0100

    meta: notes to future self

--- leco-meta.cpp
+++ leco-meta.cpp
@@ -2,6 +2,11 @@
 #include "../mct/mct-syscall.h"
 import sys;
 
+// TODO: fix hard-link bugs in OSX (my main OS, sry)
+// In an ideal world, `leco.exe` would be a hard-link to
+// `out/host-target/leco-driver.exe` so this tool would not be needed. For some
+// reason, this does not work and `leco.exe` is only a copy in OSX. I haven't
+// checked on other platforms, though.
 int main(int argc, char ** argv) try {
   if (argv[argc]) return 69;
   auto cmd = sys::tool_cmd("driver");
