515af4b4f6eac78a5e75ea7d19d720c4087f44a9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-03 17:12:29 +0100

    meta: reducing to call driver directly
    "driver" now has all the responsibility of "driving" other tools.

--- leco-meta.cpp
+++ leco-meta.cpp
@@ -4,11 +4,7 @@ import sys;
 
 int main(int argc, char ** argv) try {
   if (argv[argc]) return 69;
-
-  auto cmd = sys::tool_cmd(argv[1] ? argv[1] : "");
-  if (!mtime::of(*cmd)) cmd = sys::tool_cmd("driver");
-  else argv++;
-
+  auto cmd = sys::tool_cmd("driver");
   *argv = *cmd;
   return mct_syscall_spawn(*argv, argv);
 } catch (...) {
