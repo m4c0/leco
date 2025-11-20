56394e10415ef9358cd455e5dbd429a8e593d50f
Eduardo <m4c0@users.noreply.github.com>
2025-11-20 12:24:09 +0000

    requirements: utility to mark dirty repos

--- leco-requirements.cpp
+++ leco-requirements.cpp
@@ -3,7 +3,26 @@
 import popen;
 import sys;
 
-int main() try {
+[[noreturn]] void usage() {
+  die(R"(
+    Creates a list of LECO modules used by the current project.
+
+    Usage: ../leco/leco.exe requirements [-v]
+
+    Where:
+        -v  Marks git repo that are not clean
+)");
+}
+
+int main(int argc, char ** argv) try {
+  bool verbose = true;
+
+  const auto shift = [&] { return argc > 1 ? (argc--, *++argv) : nullptr; };
+  while (auto val = shift()) {
+    if ("-v"_s == val) verbose = true;
+    else usage();
+  }
+
   sys::strset collected {};
   collected.insert(*"../leco"_real); // Marking as dependency for any leco-based software
 
@@ -19,6 +38,17 @@ int main() try {
     p::proc p { "git", "-C", path, "rev-parse", "HEAD" };
     if (!p.gets()) die("failed to get git status");
     putan(p.last_line_read(), sim::path_filename(path));
+
+    if (verbose) {
+      p::proc p { "git", "-C", path, "status", "--porcelain=v2", "--branch" };
+      while (p.gets()) {
+        auto line = sv::unsafe(p.last_line_read());
+
+        if ("# branch.ab +0 -0"_sv == line) continue;
+        else if (line.starts_with("# branch.")) continue;
+        else putln(line);
+      }
+    }
   }
 } catch (...) {
   return 1;
