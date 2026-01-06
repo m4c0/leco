99cb6219fc1b33a676c8af864cba32c66677a389
Eduardo <m4c0@users.noreply.github.com>
2026-01-06 16:35:17 +0000

    zip: support for systems without a "zip" util

--- leco-zip.cpp
+++ leco-zip.cpp
@@ -1,5 +1,11 @@
 #pragma leco tool
+#include <stdio.h>
+
+import jojo;
+import pprent;
+import sv;
 import sys;
+import zipline;
 
 static void usage() {
   die(R"(
@@ -10,13 +16,39 @@ Usage: ../leco/leco.exe zip
 )");
 }
 
+class file_writer : public zipline::writer {
+  sys::file m_f;
+
+public:
+  explicit file_writer(const char * fname) : m_f { fname, "wb" } {}
+
+  void write(const void * ptr, unsigned sz) {
+    if (1 != fwrite(ptr, sz, 1, m_f)) die("failed to write zip file");
+  }
+  unsigned tell() {
+    return ftell(m_f);
+  }
+};
+
 static void run(const char * dag, const char * _) {
   auto path = sim::sb { dag }.path_extension("app");
   auto zip = sim::sb { dag }.path_extension("zip");
 
   sys::log("bundling", *zip);
   // TODO: use DAG to find which files should be bundled
-  sys::runf("zip -q -j %s %s/*", *zip, *path);
+
+  file_writer fw { *zip };
+  zipline::zipwriter w { &fw };
+  for (auto f : pprent::list(*path)) {
+    if (f[0] == '.') continue;
+    
+    auto fn = sv::unsafe(f);
+    if (fn.ends_with(".log")) continue;
+
+    auto fname = path / f;
+    w.write_file(fn, jojo::slurp(fname));
+  }
+  w.write_cd();
 }
 
 int main(int argc, char **argv) try {
